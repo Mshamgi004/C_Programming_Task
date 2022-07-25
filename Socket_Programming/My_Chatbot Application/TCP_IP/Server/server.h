44b42c6090642f089cd665867ef3285f9ac08af6
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-07-25 18:14:51 +0530

    Update server.h

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/server.h
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/server.h
@@ -1,25 +1,55 @@
+
+//Header files
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 
 #include<sys/socket.h>
 #include<sys/types.h>
+#include<sys/select.h>
 
 #include<netinet/in.h>
 #include<arpa/inet.h>
 
 #include<unistd.h>
-#include<netdb.h>
+#include<fcntl.h>
 
-#define PORT 8000
-#define SERVERERROR (-1)
-#define BACKLOG 4
-#define LENGTH_NAME 32
+//MACROS
+#define NO_OF_CLIENTS 10
+#define BACKLOG 5
+#define MAX_NAME_SIZE 20
+#define MAX_BUFFER_SIZE 1024
 
 
-// Function Prototypes 
-int setup_server(int port, int backlog);
-int accept_new_connection(int server_sockfd);
-int check(int exp, const char *msg);
-void *handle_connection(int client_sockfd);
+static int server_sockfd = 0;
+
+// Data structure
+struct client 
+{
+    char client_name[MAX_NAME_SIZE];
+    int file_des;
+    int port;
+    char ip[INET_ADDRSTRLEN];
+};
+
+struct server_data 
+{
+    	int total_client;
+    	struct client client_list[NO_OF_CLIENTS];
+}server;
+
+// Function declaration
+int setup_server(int *server_sockfd);
+int accept_new_connection(int server_sockfd, int *new_server_sockfd);
+void handle_connection(struct sockaddr_in client_address, int new_server_sockfd);
+void recv_from_client(int client_socket, char *recv_msg);
+void send_to_client(int client_socket, char *send_msg);
+void client_handle_list(char client_name);
+
+int server_build_fdsets(int server_sockfd, fd_set *readfds, fd_set *writefds, fd_set *exceptfds);
+int server_select(int maxval_fd, int server_sockfd, fd_set *readfds, fd_set *writefds);
+
+struct sockaddr_in server_address, client_address;
+
+
 
