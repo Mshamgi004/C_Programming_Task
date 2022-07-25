7cd6b1a4890d91f178fc3b5427a4a502b0469cb9
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-07-25 18:17:08 +0530

    Update accept_connection.c

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/accept_connection.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/accept_connection.c
@@ -1,23 +1,20 @@
 #include "server.h"
 
-int accept_new_connection(int server_sockfd)
-{	
-	int client_sockfd;
-	
-	struct sockaddr client_address;
-	
-	socklen_t addr_size;
-	addr_size = sizeof(client_address);
-	
-	client_sockfd = accept(server_sockfd, (struct sockaddr *)&client_address, &addr_size);
-	if(client_sockfd == -1)
-	{
-		printf("Error in accepting the message\n");
-		return -1;
-	}
-	else
-	{
-		printf("--------Server socket ready to accept the messages-------\n");
-	}
-	return client_sockfd;
-}
+int accept_new_connection(int server_sockfd, int *new_server_sockfd)
+{	   
+		struct sockaddr_in client_address;
+		socklen_t clilen;
+		clilen = sizeof(struct sockaddr);       // Setting the sizeof client length
+		
+		bzero(&client_address, sizeof(client_address));
+		if((*new_server_sockfd = accept(server_sockfd, (struct sockaddr *)&client_address, &clilen)) < 0);  // Accepting the pending connection
+		{
+			printf("Error in accepting the message");
+			exit(1);
+		}
+		printf(".....Server sockfd is ready to accept the incoming messages....\n");
+		
+		handle_connection(client_address,*new_server_sockfd);
+		
+		return 0;
+}		
