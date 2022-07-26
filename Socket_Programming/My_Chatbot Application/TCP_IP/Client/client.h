78c01c6c9b8cc747a07069cf379a57fe0a896c2b
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-07-26 18:23:32 +0530

    Used return type integer to void for function declaration in send and recv

--- Socket_Programming/My_Chatbot Application/TCP_IP/Client/client.h
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Client/client.h
@@ -14,14 +14,16 @@
 #include<fcntl.h>
 
 // MACROS
-#define PORT 8000
+#define PORT 8000 
 #define MAX_NAME_SZE 20
 #define MAX_BUFFER_SIZE 1024
 
-char client_name[MAX_BUFFER_SIZE];
+char client_name[MAX_BUFFER_SIZE];      // Char buffer used to store the client name
 
+// Function declaration 
 int setup_client(int *client_sockfd);
-void client_recv_from_server(int client_socket, char *recv_msg);
-void client_send_to_server(int client_socket, char *send_msg);
-int server_build_fdsets(int client_sockfd,fd_set *readfds,fd_set *writefds,fd_set *exceptfds);
+int client_recv_from_server(int client_socket, char *recv_msg);
+int client_send_to_server(int client_socket, char *send_msg);
+int client_build_fdsets(int client_sockfd,fd_set *readfds,fd_set *writefds,fd_set *exceptfds);
 int client_select(int maxval_fd, int client_sockfd,fd_set *readfds,fd_set *writefds);
+
