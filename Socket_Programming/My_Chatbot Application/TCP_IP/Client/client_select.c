b93e3ef2d57847147002c15bbd61cc2f46f65650
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-07-26 18:10:33 +0530

    Update client_select.c

--- Socket_Programming/My_Chatbot Application/TCP_IP/Client/client_select.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Client/client_select.c
@@ -1,31 +1,51 @@
 #include "client.h"
 
+// Function declaration for client_select
 int client_select(int maxval_fd, int client_sockfd,fd_set *readfds,fd_set *writefds)
 {
-	char recv_msg[MAX_BUFFER_SIZE];
+	// Declaring the char buffer for storing the sending and recieving message
+	char recv_msg[MAX_BUFFER_SIZE];    
 	char send_msg[MAX_BUFFER_SIZE];
+	// Clearing the buffer initially to zero
+	memset(recv_msg,0,sizeof(recv_msg));
+	memset(send_msg,0,sizeof(send_msg));
 
-	bzero(recv_msg,sizeof(recv_msg));
-	bzero(send_msg,sizeof(send_msg));
-
-	int client_action = select(maxval_fd + 1, readfds, writefds, NULL, NULL);
+	int client_action = select(maxval_fd + 1, readfds, writefds, NULL, NULL);  // select() system call 
 	if(client_action == -1 || client_action == 0)
 	{
 		printf("ERROR: select");
 		exit(1);
 	}
 
+	// To check for all the clients in the file des inside the readfds
 	if(FD_ISSET(client_sockfd, readfds))
 	{
+		// If its present in readfds then it will recieve the message
 		client_recv_from_server(client_sockfd, recv_msg);	
 	}
-
-	if(FD_ISSET(STDIN_FILENO,readfds))
+	else
 	{
+		printf("Not able to recv the message\n");
+	}
+
+	// To check the fd is set for sending the message so as to broadcast the message to all clients
+	if(FD_ISSET(0,readfds))
+	{
+
 		if(read(0,send_msg,sizeof(send_msg)) > 0)
 		{
 			client_send_to_server(client_sockfd,send_msg);
 		}
+		else
+		{
+			printf("Not able to send the message\n");
+		}
 	}
+	else
+	{
+		printf("ERROR: in setting the readfds\n");
+	}
+	
 	return 0;
 }
+
