487ecff101850a573bd186bd79e5e69d6bad188e
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-07-26 17:44:56 +0530

    Updated some of the function definations and working on process_recv_data function. To be commited

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/server.h
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/server.h
@@ -1,4 +1,3 @@
-
 //Header files
 #include<stdio.h>
 #include<stdlib.h>
@@ -16,22 +15,25 @@
 
 //MACROS
 #define NO_OF_CLIENTS 10
+#define PORT 8000
 #define BACKLOG 5
 #define MAX_NAME_SIZE 20
 #define MAX_BUFFER_SIZE 1024
 
+static int server_sockfd = 0;    // Static global variable of server_sockfd
 
-static int server_sockfd = 0;
-
-// Data structure
+// Structure to hold the name, ip-address, port and file des 
 struct client 
 {
     char client_name[MAX_NAME_SIZE];
+    char chatwith[MAX_NAME_SZE];
+    int chatwith_fd;
     int file_des;
     int port;
     char ip[INET_ADDRSTRLEN];
 };
 
+// Data structure to hold the total no of clients
 struct server_data 
 {
     	int total_client;
@@ -42,14 +44,13 @@ struct server_data
 int setup_server(int *server_sockfd);
 int accept_new_connection(int server_sockfd, int *new_server_sockfd);
 void handle_connection(struct sockaddr_in client_address, int new_server_sockfd);
-void recv_from_client(int client_socket, char *recv_msg);
-void send_to_client(int client_socket, char *send_msg);
-void client_handle_list(char client_name);
-
+int recv_from_client(int client_socket, char *recv_msg);
+int send_to_client(int client_socket, char *send_msg);
 int server_build_fdsets(int server_sockfd, fd_set *readfds, fd_set *writefds, fd_set *exceptfds);
 int server_select(int maxval_fd, int server_sockfd, fd_set *readfds, fd_set *writefds);
-
-struct sockaddr_in server_address, client_address;
-
-
-
+/*
+ Working 
+int process_recv_data(int socket,char *client_buffer);
+int find_the_client_index_list(int socket);
+int find_the_client_index_by_name(char *client_name);
+*/
