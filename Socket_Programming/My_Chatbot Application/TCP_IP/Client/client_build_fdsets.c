ec8d057ee65cd38fd61d8db22e3bcc1f49f75f8b
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-07-26 18:17:54 +0530

    Added comments

--- Socket_Programming/My_Chatbot Application/TCP_IP/Client/client_build_fdsets.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Client/client_build_fdsets.c
@@ -1,14 +1,15 @@
 #include "client.h"
 
-int server_build_fdsets(int client_sockfd,fd_set *readfds,fd_set *writefds,fd_set *exceptfds)
+// Function defination for client_build_fdsets
+int client_build_fdsets(int client_sockfd,fd_set *readfds,fd_set *writefds,fd_set *exceptfds)
 {
-	int maxval_fd = client_sockfd;
+	int maxval_fd = client_sockfd;   // Keeping the value of client_sockfd = maxval_fd
 
-	FD_ZERO(readfds);
-	FD_SET(client_sockfd, readfds);
-	FD_SET(client_sockfd, writefds);
-	FD_SET(STDIN_FILENO, readfds);
-	fcntl(STDIN_FILENO, F_SETFL, O_NONBLOCK);
+	FD_ZERO(readfds);              // Initializing readfds with zero value
+	FD_SET(client_sockfd, readfds);    // Setting the client_sockfd as readfds
+	FD_SET(client_sockfd, writefds);   // Setting the readfds to a 0 value so as to capture in readfdsq
+	FD_SET(0, readfds);               // Setting the readfds to 0 so as to recive the commands from cmd
+	fcntl(0, F_SETFL, O_NONBLOCK);    // fcntl() used to nonblocking the application from the cmd
 
 	return maxval_fd;
 }
