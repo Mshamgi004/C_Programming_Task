f1efb18868d249b9f417130e18311c3da988ca0e
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-07-26 17:56:03 +0530

    Changed the logic of send to client

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/send_to_client.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/send_to_client.c
@@ -1,15 +1,23 @@
 #include "server.h"
 
-
-void send_to_client(int client_socket, char *send_msg)
+// Function defination to send the message to client
+int send_to_client(int client_socket, char *send_msg)
 {
+	int write_bytes = 0;   // Variable used for writing the message
+	//bzero(send_msg, sizeof(send_msg));     // bzero() used to erase the data stored in buffer 
+	socklen_t servlen;
+	servlen = strlen(send_msg);					
+	//printf("Enter message to be send to server: %s ",send_msg);
 				
-	int write_bytes = 0;
-	bzero(send_msg, sizeof(send_msg));     // bzero() used to erase the data stored in buffer 
-						
-	printf("Enter message to be send to server: %s ",send_msg);
-	// gets(send_msg);
-				
-	write_bytes = send(server_sockfd, send_msg, strlen(send_msg), 0); 
-	printf("\n[CLIENT : %d] || Wrote [%d] number of bytes || BYTES = [%s]\n",server_sockfd,write_bytes, send_msg);
-}	
+	if((write_bytes = send(client_socket, send_msg, servlen, 0)) > 0) // send() message
+	{
+		printf("\n[CLIENT : %d] || BYTES = [%s]\n",server_sockfd,send_msg);
+	}
+	else
+	{
+		printf("Error : send failed");
+		return -1;
+	}
+	return write_bytes;
+
+}
