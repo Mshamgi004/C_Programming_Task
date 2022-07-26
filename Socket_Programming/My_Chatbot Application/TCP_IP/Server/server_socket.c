63b27de40b9047f300c1c7925c9166b845102eb5
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-07-26 17:46:50 +0530

    Updated the binding operation which wasn't working.

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/server_socket.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/server_socket.c
@@ -1,54 +1,49 @@
 #include "server.h"
 
-int setup_server(int port, int backlog)
+// Function defination to setup the server socket fd
+int setup_server(int *server_sockfd)
 {
-	int opt_port = 1;
-	int bind_check;
-	int server_sockfd;
+	int opt_port;          // Variable used for setsockopt()
+	//int bind_check;        // Variable used for bind check
 	
-	struct sockaddr_in server_address;
-	char server_responce[1024];
+	struct sockaddr_in server_address; // Structure describing the internet sockaddr
 	
-	socklen_t addr_size;
-	addr_size = sizeof(server_responce);
-	
-	server_sockfd = socket(AF_INET,SOCK_STREAM,0);
-	if(server_sockfd == -1)
+	if((*server_sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) // Creating a server socket                         // Checking for server_socket is created or not 
 	{
 		printf("Error in creating the socket");
-		return -1;
+		exit(1);
 	}
 	else
 	{
-		printf("-----Socket created successfully-----\n");
+		printf(".......Server Socket created succesfully !...... \n");
 	}
-	
-	if(setsockopt(server_sockfd, SOL_SOCKET, (SO_REUSEADDR | SO_REUSEPORT), &opt_port , sizeof(opt_port)));  // Forcefully sets the socket port number	
+	//bzero(&server_address, sizeof(server_address));    //bzero clears any data 
+	// Declaring the family, port and address for sockfd
+	server_address.sin_family = AF_INET;    
+	server_address.sin_port = htons(PORT);   
+	server_address.sin_addr.s_addr = INADDR_ANY;
 
-	bzero(&server_address, sizeof(server_address));
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(PORT);
-	server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
-	// Checking for the address matching
-	// if (INADDR_NONE == addr_size);
-  	// {
-    // 	perror("inet_addr() failed");
-    // 	exit(1); /* or whatever error handling you choose. */
-  	// }
+	setsockopt(*server_sockfd, SOL_SOCKET, (SO_REUSEADDR|SO_REUSEPORT), &opt_port , sizeof(opt_port)); // Forcefully sets the socket port number
 
-	bind_check = bind(server_sockfd, (struct sockaddr *)&server_address, sizeof(server_address));
-	if(bind_check == -1)
+	if(0 != bind(*server_sockfd, (struct sockaddr *)&server_address, sizeof(struct sockaddr))) // Binding the socket
 	{
-		printf("Error in binding");
-		return -1;
+		printf("Error in binding \n");
+	 	exit(1);
 	}
 	else
 	{
-		printf("--------Binding successfull--------\n");
-		printf("The port is binded to : %d\n", PORT);
+		printf("Socket binded succefully to port no : %d!\n", server_address.sin_port);
 	}
 	
-	listen(server_sockfd, BACKLOG);
-
-	return server_sockfd;
+	if(0 != listen(*server_sockfd, BACKLOG))       // Listening the incoming connection
+	{
+		printf("Error in listening to the messages in queue \n");
+		exit(1);
+	}
+	else
+	{
+		printf(".......Socket is listening....... \n");
+	}
+	return 0;
 }	
+
