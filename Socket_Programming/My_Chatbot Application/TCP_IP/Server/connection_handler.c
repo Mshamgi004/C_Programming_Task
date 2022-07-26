d7e768813a01bb9e9974b4149f040e66428b0687
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-07-26 17:52:15 +0530

    Updated the working of reciveing the client name

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/connection_handler.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/connection_handler.c
@@ -1,26 +1,32 @@
 #include "server.h"
 
+// Function defination to handle the client
 void handle_connection(struct sockaddr_in client_address, int new_server_sockfd)
 {
-    char ip[INET_ADDRSTRLEN] = {0};
-    char client_buffer[MAX_BUFFER_SIZE] = {0};
-    char recv_msg[32];
+    char ip[INET_ADDRSTRLEN] = {0};       // char buffer to store the ip address of connected clients
+    char client_buffer[MAX_BUFFER_SIZE] = {0};   // char buffer to store the name of connected clients
+    char recv_msg[32];   // char buffer to store the reciveing message
 	
-	//get extra server details
-	recv_from_client(new_server_sockfd, client_buffer);
+	recv_from_client(new_server_sockfd, client_buffer);  // Function call to recieve the name from client
     
    
-	//get the IP and Port client details
+	//get the name, IP and Port client details
     int port = ntohs(client_address.sin_port);
     inet_ntop(AF_INET, &(client_address.sin_addr), ip, INET_ADDRSTRLEN);
+    printf("CLIENT NAME = %s\n",client_buffer);
     printf("[CLIENT-INFO] : [port = %d , ip = %s]\n",port, ip);
-   
+	
+	// To check for the no. of clients connected 
     if(server.total_client >= NO_OF_CLIENTS) 
 	{
         perror("----No more space for client to connect to the server----\n");
     }
+	else
+	{
+		printf("YES: client can be connected \n");
+	}
 	
-	//populate the new client data 
+	//populate the new client data and saving it in client_list
    	strncpy(server.client_list[server.total_client].client_name,client_buffer,strlen(client_buffer));
     server.client_list[server.total_client].port = port;
     strcpy(server.client_list[server.total_client].ip, ip);
@@ -29,5 +35,3 @@ void handle_connection(struct sockaddr_in client_address, int new_server_sockfd)
     server.total_client++;
     
 }
-		
-			
