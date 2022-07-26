258439424428d09d5aa59672fc1b7754bbde4244
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-07-26 17:57:27 +0530

    Added memset() to clear the buffer initially

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/recv_from_client.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/recv_from_client.c
@@ -1,16 +1,19 @@
 #include "server.h"
 
-void recv_from_client(int client_socket, char *recv_msg)
+// Function defination for recieving the name from client
+int recv_from_client(int client_socket, char *recv_msg)
 {
-	int read_bytes = 0;
-	bzero(recv_msg,strlen(recv_msg));
+	int read_bytes = 0;    // Variable use to store the read_bytes
+	memset(recv_msg,0,strlen(recv_msg));     // Clearing the buffer initially
 
+	// recv() function used to recieve the name of client
 	if((read_bytes = recv(client_socket, recv_msg, MAX_BUFFER_SIZE, 0 )) > 0)
-	{
-		printf("Server is ready to recieve message \n");
+	{ 
+		printf("Server is ready to recieve the name \n");
 	}
 	else
 	{	
-		printf("Error in recieveing the message\n");
+		printf("Error in recieveing the name \n");
 	}
+	return 0;
 }	
