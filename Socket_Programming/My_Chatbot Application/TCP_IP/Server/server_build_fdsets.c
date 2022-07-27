d7272ce634f4b71c124d020b2288d9ae4957d346
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-07-27 18:16:37 +0530

    Introduced STDIN_FILENO in fd_set for setting the initial value to zero

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/server_build_fdsets.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/server_build_fdsets.c
@@ -1,20 +1,20 @@
 #include "server.h"
 
 // Function defination of server_build_fdsets
-int server_build_fdsets(int server_sockfd, fd_set *readfds, fd_set *writefds, fd_set *exceptfds)
+int server_build_fdsets(int server_sockfd, fd_set *readfds, fd_set *writefds, fd_set *exceptfds) 
 {
-	int maxval_fd = server_sockfd;               // Keeping the value of server_sockfd = maxval_fd
+	int maxval_fd = server_sockfd;   // Keeping the value of server_sockfd = maxval_fd
 
-	FD_ZERO(readfds);				// Initializing readfds with zero value
-	FD_SET(server_sockfd, readfds);             // Setting the server_sockfd as readfds
-	FD_SET(0, readfds);              		// Setting the readfds to a 0 value so as to capture in readfdsq
-	fcntl(0, F_SETFL, O_NONBLOCK);              // fcntl() used to nonblocking the application from the cmd
-
+	FD_ZERO(readfds);  //Initializing readfds with zero value
+	FD_SET(server_sockfd, readfds);  // Setting the server_sockfd as readfds
+	FD_SET(STDIN_FILENO,readfds);   // Setting the readfds to a 0 value so as to capture in readfdsq
+	fcntl(STDIN_FILENO,F_SETFL,O_NONBLOCK);   // fcntl() used to nonblocking the application from the cmd 
+	
 	// Loop use to fill the readfds with all the socket fd's 
-	for(int val_read = 0; val_read < server.total_client; val_read++)     
+	for(int val = 0; val < server.total_client; val++) 
 	{
-		FD_SET(server.client_list[val_read].file_des, readfds);
-		maxval_fd++;
-	}
-	return maxval_fd;
+        FD_SET(server.client_list[val].file_des,readfds);
+        maxval_fd++;
+    }
+    return maxval_fd;
 }
