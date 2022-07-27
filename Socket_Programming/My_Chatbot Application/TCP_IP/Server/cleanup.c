5f54d32c07e2c243a0389b5a310203f2f124d7a4
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-07-28 00:39:51 +0530

    Update cleanup.c

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/cleanup.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/cleanup.c
@@ -5,6 +5,6 @@ void cleanup()
     close(server_sockfd);
     for(int clean = 0; clean < server.total_client; clean++) 
     {
-        close(server.client_list[i].file_des);
+        close(server.client_list[clean].file_des);
     }
 }
