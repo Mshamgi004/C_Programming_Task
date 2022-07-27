58a9badddd66162143b88be84733b9e044d667a6
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-07-28 00:37:20 +0530

    Update client_send_to_server.c

--- Socket_Programming/My_Chatbot Application/TCP_IP/Client/client_send_to_server.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Client/client_send_to_server.c
@@ -4,12 +4,12 @@
 int client_send_to_server(int client_socket, char *send_msg)
 {	
 	int write_bytes = 0;      // Variable used for writing the message
-	socklen_t serlen;         // Declaring a socklen_t variable -> serlen
-	serlen = strlen(send_msg);     // Storing the send buffer in serlen
+	int clilen;         // Declaring a socklen_t variable -> serlen
+	clilen = strlen(send_msg);     // Storing the send buffer in serlen
 		
 	if((write_bytes = send(client_socket, send_msg, serlen,0)) < 0)  // send() message to server 
 	{				
-		printf("Error: Send failed\n");
+		printf("****Error: send() failed****\n");
 		exit(1);
 	}
 	else
