98908090dff6df3c7af776e4525f84be26fabe32
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-07-28 00:39:18 +0530

    Update client_recv_from_server.c

--- Socket_Programming/My_Chatbot Application/TCP_IP/Client/client_recv_from_server.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Client/client_recv_from_server.c
@@ -4,20 +4,19 @@
 int client_recv_from_server(int client_socket, char *recv_msg)
 {
 		int read_bytes = 0;      // Variable use to store the read_bytes 
-		//bzero(recv_msg, sizeof(recv_msg));     // bzero() used to erase the data stored in buffer 
-					
+		
 		if((read_bytes = recv(client_socket, recv_msg, MAX_BUFFER_SIZE, 0)) > 0)   // recv() message from server
 		{
 			printf("%s\n",recv_msg);
 		}	  
 		else if(read_bytes == 0)
 		{
-			printf("Client Disconnected\n");
+			printf("*****Client Disconnected*****\n");
 			close(client_socket);
 		}
 		else
 		{
-			printf("ERROR: recv failes\n");
+			printf("*****ERROR: recv() failes******\n");
 		}
 		return 0;
 }
