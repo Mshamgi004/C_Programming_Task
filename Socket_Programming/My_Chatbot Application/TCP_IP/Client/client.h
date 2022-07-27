eb2dddd2e8a3c7f798d4916d6fb20e8c73c4eb32
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-07-28 00:25:28 +0530

    Update client.h

--- Socket_Programming/My_Chatbot Application/TCP_IP/Client/client.h
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Client/client.h
@@ -14,16 +14,15 @@
 #include<fcntl.h>
 
 // MACROS
-#define PORT 8000 
+#define SERVER_PORT 9000
 #define MAX_NAME_SZE 20
 #define MAX_BUFFER_SIZE 1024
 
-char client_name[MAX_BUFFER_SIZE];      // Char buffer used to store the client name
+char client_name[MAX_NAME_SZE];   // Char buffer used to store the client name
 
-// Function declaration 
-int setup_client(int *client_sockfd);
-int client_recv_from_server(int client_socket, char *recv_msg);
-int client_send_to_server(int client_socket, char *send_msg);
-int client_build_fdsets(int client_sockfd,fd_set *readfds,fd_set *writefds,fd_set *exceptfds);
-int client_select(int maxval_fd, int client_sockfd,fd_set *readfds,fd_set *writefds);
-
+//Function declaration
+int client_create_socket(int *client_sockfd);
+int client_recv_from_server(int socket_client, char *recv_msg);
+int client_send_to_server(int socket_client, char *send_msg);
+int client_build_fdsets(int listenfd, fd_set *readfds, fd_set *writefds, fd_set *exceptfds);
+int client_select(int maxval_fd, int listenfd, fd_set *readfds, fd_set *writefds);
