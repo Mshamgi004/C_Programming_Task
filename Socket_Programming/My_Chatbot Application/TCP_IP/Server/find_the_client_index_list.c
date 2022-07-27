1b48d41067eba9e1191b8b47ac38b9e6ee751d43
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-07-27 18:22:49 +0530

    Created it for searching the index value of client from server

---
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/find_the_client_index_list.c
@@ -0,0 +1,15 @@
+#include "server.h"
+
+// Function defination of find index of the client data structure from client socket
+int find_the_client_index_list(int socket) 
+{
+	int index = 0;
+	for(int index_val = 0; index_val < server.total_client; index_val++) 
+	{
+		if(server.client_list[index_val].file_des == socket) 
+		{
+			index = index_val;
+        }
+    }
+    return index;
+}
