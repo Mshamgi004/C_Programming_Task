04de034ee47f5943ac69df38191e0bab5692c59d
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-07-27 18:12:14 +0530

    Added comments

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/accept_connection.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/accept_connection.c
@@ -1,25 +1,25 @@
 #include "server.h"
 
 // Function defination for accepting the new connection
-int accept_new_connection(int server_sockfd, int *new_server_sockfd)
-{	   
-		struct sockaddr_in client_address;   // struct sockaddr_in in client_address
-		socklen_t clilen;                    // Creating  a socklen_t variable -> clilen
-		clilen = sizeof(struct sockaddr);       // Setting the sizeof client length
-		
-		bzero(&client_address, sizeof(client_address));   // Clearing the client address initially
-		if((*new_server_sockfd = accept(server_sockfd, (struct sockaddr *)&client_address, &clilen)) < 0)  // Accepting the pending connection
-		{
-			printf("Error in accepting the message");
-			exit(1);
-		}
-		else
-		{
-			printf(".....Server sockfd is ready to accept the incoming messages....\n");
-			handle_connection(client_address,*new_server_sockfd);   // Function call to populate the ip and port for handling the connection 
-		}
-		
-		//handle_connection(client_address,*new_server_sockfd);   // Function call to populate the ip and port for handling the connection 
-		
-		return 0;
-}		
+int accept_new_connection(int server_sockfd, int *new_server_sockfd) 
+{
+    struct sockaddr_in client_address;  // struct sockaddr_in in client_address
+    int server_length;    // Creating a variable -> server_length
+    server_length = sizeof(struct sockaddr);   // assigning the address to the variable server_length
+    
+	bzero(&client_address,sizeof(client_address));  // Clearing the client address initially
+        
+    if((*new_server_sockfd = accept(server_sockfd, (struct sockaddr*)&client_address, &server_length)) < 0)  // Accepting the pending connection
+    {
+        printf("******ERROR :Accept failed*******");
+        return -1;
+    }
+	else
+	{	
+		printf("----Socket is ready to accept the incoming client details----\n");
+		printf("-------------------------------------------------------------\n");
+		handle_connection(client_address ,*new_server_sockfd);  // Function call to populate the ip and port for handling the connection 
+	}
+    
+    return 0;
+}
