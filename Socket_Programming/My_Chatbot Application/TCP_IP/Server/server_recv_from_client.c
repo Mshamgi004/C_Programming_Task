5b47d6310c3c47b3167484c31b0b25f41b4812de
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-07-27 18:18:45 +0530

    Update and rename recv_from_client.c to server_recv_from_client.c

---
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/server_recv_from_client.c
@@ -0,0 +1,23 @@
+#include "server.h"
+
+// Function defination for recieving the name from client
+int server_recv_from_client(int client_socket, char *recv_msg) 
+{
+	int read_bytes = 0;   // Variable use to store the read_bytes
+	memset(recv_msg,0,strlen(recv_msg));  // Clearing the buffer initially
+    
+	if((read_bytes = recv(client_socket, recv_msg, MAX_BUFFER_SIZE, 0)) > 0) 
+	{
+		process_recv_data(client_socket, recv_msg);
+    }
+    else if(read_bytes == 0) 
+    {
+		printf("*****Client Disconnected*****\n");
+	}
+    else 
+    {
+		printf("*****ERROR: recv() failed*****\n");
+	}
+    
+	return 0;
+}
