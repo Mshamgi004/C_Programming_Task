b8e363b9ca7ad3960b5a1942c717930cad5e61e4
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-07-27 18:13:18 +0530

    Completed the working on recieving the name, port and ip address

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/connection_handler.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/connection_handler.c
@@ -1,37 +1,33 @@
 #include "server.h"
 
 // Function defination to handle the client
-void handle_connection(struct sockaddr_in client_address, int new_server_sockfd)
+void handle_connection(struct sockaddr_in client_information, int new_server_sockfd) 
 {
-    char ip[INET_ADDRSTRLEN] = {0};       // char buffer to store the ip address of connected clients
-    char client_buffer[MAX_BUFFER_SIZE] = {0};   // char buffer to store the name of connected clients
-    char recv_msg[32];   // char buffer to store the reciveing message
-	
-	recv_from_client(new_server_sockfd, client_buffer);  // Function call to recieve the name from client
-    
+	char ip[INET_ADDRSTRLEN] = {0};  // char buffer to store the ip address of connected clients
+	char client_buffer[MAX_BUFFER_SIZE] = {0};   // char buffer to store the name of connected clients
+
+    server_recv_from_client(new_server_sockfd,client_buffer);  // Function call to recieve the name from client
    
-	//get the name, IP and Port client details
-    int port = ntohs(client_address.sin_port);
-    inet_ntop(AF_INET, &(client_address.sin_addr), ip, INET_ADDRSTRLEN);
-    printf("CLIENT NAME = %s\n",client_buffer);
-    printf("[CLIENT-INFO] : [port = %d , ip = %s]\n",port, ip);
-	
-	// To check for the no. of clients connected 
+	//get the name IP address and port no: client 
+    int port = ntohs(client_information.sin_port);
+    inet_ntop(AF_INET, &(client_information.sin_addr), ip, INET_ADDRSTRLEN);
+	printf("---------------------------------\n");
+    printf("\n[CLIENT NAME] : %s\n", client_buffer);
+	printf("---------------------------------\n");
+    printf("\n[CLIENT-INFO] : [port] = %d , [IP Address] = %s\n", port, ip);
+	printf("---------------------------------\n");
+   	
+   	// To check for the no. of clients connected 
     if(server.total_client >= NO_OF_CLIENTS) 
-	{
-        perror("----No more space for client to connect to the server----\n");
+    {
+        printf("****ERROR : No more space for client to save****\n");
     }
-	else
-	{
-		printf("YES: client can be connected \n");
-	}
 	
 	//populate the new client data and saving it in client_list
-   	strncpy(server.client_list[server.total_client].client_name,client_buffer,strlen(client_buffer));
+    strncpy(server.client_list[server.total_client].client_name ,client_buffer,strlen(client_buffer));
     server.client_list[server.total_client].port = port;
     strcpy(server.client_list[server.total_client].ip, ip);
-    server.client_list[server.total_client].file_des =  new_server_sockfd;
+    server.client_list[server.total_client].file_des = new_server_sockfd;
     
     server.total_client++;
-    
 }
