b087f24067f85cb146041c5542d3ea78704d0140
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-07-27 18:08:31 +0530

    Added three functions such to process the receive data so as to communicate within clients.

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/server.h
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/server.h
@@ -3,8 +3,6 @@
 #include<stdlib.h>
 #include<string.h>
 
-#include<sys/socket.h>
-#include<sys/types.h>
 #include<sys/select.h>
 
 #include<netinet/in.h>
@@ -13,44 +11,45 @@
 #include<unistd.h>
 #include<fcntl.h>
 
-//MACROS
+//Macros
+#define SERVER_PORT 9000
+#define LISTEN_BACKLOG 5
+#define MAX_NAME_SZE 20
 #define NO_OF_CLIENTS 10
-#define PORT 8000
-#define BACKLOG 5
-#define MAX_NAME_SIZE 20
 #define MAX_BUFFER_SIZE 1024
+#define CONNECTED "Connected ....."
 
-static int server_sockfd = 0;    // Static global variable of server_sockfd
+static int server_sockfd = 0;  // Static global variable of server_sockfd
 
 // Structure to hold the name, ip-address, port and file des 
 struct client 
 {
-    char client_name[MAX_NAME_SIZE];
-    char chatwith[MAX_NAME_SZE];
-    int chatwith_fd;
-    int file_des;
-    int port;
-    char ip[INET_ADDRSTRLEN];
+	char client_name[MAX_NAME_SZE];
+	char chatwith[MAX_NAME_SZE];
+	int chatwith_fd;
+	int file_des;
+	int port;
+	char ip[INET_ADDRSTRLEN];
 };
 
 // Data structure to hold the total no of clients
 struct server_data 
 {
-    	int total_client;
-    	struct client client_list[NO_OF_CLIENTS];
-}server;
+	int total_client;
+	struct client client_list[NO_OF_CLIENTS];
+};
 
-// Function declaration
+struct server_data server;
+
+//Function declarations
 int setup_server(int *server_sockfd);
 int accept_new_connection(int server_sockfd, int *new_server_sockfd);
-void handle_connection(struct sockaddr_in client_address, int new_server_sockfd);
-int recv_from_client(int client_socket, char *recv_msg);
-int send_to_client(int client_socket, char *send_msg);
+int server_recv_from_client(int socket_client, char *recv_msg);
+int server_send_to_client(int socket_client, char *send_msg);
 int server_build_fdsets(int server_sockfd, fd_set *readfds, fd_set *writefds, fd_set *exceptfds);
 int server_select(int maxval_fd, int server_sockfd, fd_set *readfds, fd_set *writefds);
-/*
- Working 
-int process_recv_data(int socket,char *client_buffer);
+void handle_connection(struct sockaddr_in client_information, int new_server_sockfd);
+int process_recv_data(int socket,char *buffer);
 int find_the_client_index_list(int socket);
-int find_the_client_index_by_name(char *client_name);
-*/
+int find_the_client_index_by_name(char *name);
+void cleanup(void);
