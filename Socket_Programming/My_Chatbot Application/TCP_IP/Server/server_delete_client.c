2ed95f7c8c070672d206c7397e7435312bf24087
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-07-28 18:07:11 +0530

    Created a delete call as my Server was not handling termination from client

---
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/server_delete_client.c
@@ -0,0 +1,19 @@
+#include "server.h"
+
+// Function defination to delete the client
+void server_delete_client(int socket_fd_delete) 
+{
+    int delete = 0;
+    int index = 0;
+
+	// Iterating the variable till tital no. of clients
+    for(delete = 0; delete < NO_OF_CLIENTS; delete++) 
+	{
+		// Condition for deleting the client
+        if(server.client_list[delete].file_des == socket_fd_delete) 
+		{
+			printf("-------Socket deleted = [%d]\n",socket_fd_delete);
+        }
+    }
+    close(socket_fd_delete);
+}
