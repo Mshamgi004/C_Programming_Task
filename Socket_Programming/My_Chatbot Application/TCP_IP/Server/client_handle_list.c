4412539fa1b0f3fa6f71fee58e8261ecb458537d
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-07-29 14:11:21 +0530

    Working on the this file as name and ip address are being stored on text file

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/client_handle_list.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/client_handle_list.c
@@ -1,17 +1,10 @@
 #include "server.h"
 
-// Fucntion defination to store the client details in the (.txt) file
-
-// *****************************WORKING*********************************//
-void client_handle_list(struct client detail)
+// Function call to handle the text file in server side to read the contents of client
+/************************************************WORKING****************************************/
+void client_handle_list(char *client_buffer, char *ip)
 {	
-	struct client_file
-	{
-		char client_buffer[MAX_BUFFER_SIZE];
-		char ip[INET_ADDRSTRLEN];
-		int port;
-	}list;
-
+	int val_read;
    	FILE *fptr;
    	fptr = fopen("CLIENT_INFO.txt", "w");
    	if (fptr == NULL) 
@@ -19,32 +12,107 @@ void client_handle_list(struct client detail)
     	fprintf(stderr, "\nError to open the file\n");
       	exit (1);
    	}
-
-   	fwrite (&list, sizeof(struct client_file), 1, fptr);
-   	
-   	if(fwrite != 0)
+	else
 	{
-		printf("Contents to file written successfully !\n");
+		printf("FILE OPENED SUCCESSUFULLY\n");
+	} 
+	
+	//fwrite (&detail, sizeof(struct client), 1, fptr);
+	if(strlen(client_buffer) > 0)
+	{
+		// writing in the file using fputs()
+		fputs(client_buffer, fptr) ;
+		fputs("\n", fptr) ;
 	}
-    else
+	else
 	{
-    	printf("Error writing file !\n");
-	}	
-   	fclose (fptr);
+		printf("Not able to print the name\n");
+	}
+
+	if(strlen(ip) > 0)
+	{
+		// writing in the file using fputs()
+		fputs(ip, fptr) ;
+		fputs("\n", fptr) ;
+	}
+	else
+	{
+		printf("Not able to print the ip address\n");
+	}
+	// if(strlen(port) > 0)
+	// {
+	// 	// writing in the file using fputs()
+	// 	fputs(port, fptr) ;
+	// 	fputs("\n", fptr) ;
+	// }
+	// else
+	// {
+	// 	printf("Not able to print the port no\n");
+	// }
+	
+	
+	
+   	// if(fwrite != 0)
+	// {
+	// 	printf("Contents to file written successfully !\n");
+	// }
+    // else
+	// {
+    // 	printf("Error writing file !\n");
+	// }	
+   	fclose(fptr);
    	
-	FILE *fptr1;
-   	
-   	fptr1 = fopen ("CLIENT_INFO.txt", "r");
-   	if (fptr1 == NULL) 
+	fptr = fopen ("CLIENT_INFO.txt", "r");
+   	if (fptr == NULL) 
 	{
       	fprintf(stderr, "\nError to open the file\n");
-      	exit (1);
+      	exit(1);
    	}
-  	while(fread(&list, sizeof(struct client_file), 1, fptr1))
+	
+	for(val_read = 0; val_read < server.total_client; val_read++)
 	{
-    	printf("NAME = %s\n", list.client_buffer);
-		printf("IP Address = %s\n", list.ip);
-		printf("Port = %d\n", list.port);
-	}	
-   	fclose (fptr1);
+		printf("Started reading\n");
+		//while(fread(&detail, sizeof(struct client), 1, fptr1))
+		while(fgets(client_buffer,MAX_BUFFER_SIZE,fptr) != NULL)
+		{
+    	// printf("NAME = %s\n", detail.client_name);
+		// printf("IP Address = %s\n", detail.ip);
+		// printf("Port = %d\n", detail.port);
+			printf("NAME = %s\n",client_buffer);
+		}
+
+		while(fgets(ip,INET_ADDRSTRLEN,fptr) != NULL)
+		{
+    		// printf("NAME = %s\n", detail.client_name);
+			printf("IP Address = %s\n", ip);
+			// printf("Port = %d\n", detail.port);
+		
+		}
+	}
+
+  	// //while(fread(&detail, sizeof(struct client), 1, fptr1))
+	// while(fgets(client_buffer,MAX_BUFFER_SIZE,fptr) != NULL)
+	// {
+    // 	// printf("NAME = %s\n", detail.client_name);
+	// 	// printf("IP Address = %s\n", detail.ip);
+	// 	// printf("Port = %d\n", detail.port);
+	// 	printf("NAME = %s\n",client_buffer);
+	// }
+
+	// while(fgets(ip,INET_ADDRSTRLEN,fptr) != NULL)
+	// {
+    // 	// printf("NAME = %s\n", detail.client_name);
+	// 	printf("IP Address = %s\n", ip);
+	// 	// printf("Port = %d\n", detail.port);
+		
+	// }
+	
+	
+
+	// while(fgets(port,MAX_BUFFER_SIZE,fptr) != NULL)
+	// {
+    // 	printf("Port = %s\n", port);
+  	// }
+   	fclose (fptr);
 }
+
