17c43f5bf5db391467c6583eede3160563fd4eec
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-07-29 07:55:03 +0530

    Include the server_delete_client.c call after client disconnection

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/server_recv_from_client.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/server_recv_from_client.c
@@ -6,6 +6,7 @@ int server_recv_from_client(int client_socket, char *recv_msg)
 	int read_bytes = 0;   // Variable use to store the read_bytes
 	memset(recv_msg,0,strlen(recv_msg));  // Clearing the buffer initially
     
+	// recv() call
 	if((read_bytes = recv(client_socket, recv_msg, MAX_BUFFER_SIZE, 0)) > 0) 
 	{
 		process_recv_data(client_socket, recv_msg);
@@ -13,6 +14,7 @@ int server_recv_from_client(int client_socket, char *recv_msg)
     else if(read_bytes == 0) 
     {
 		printf("*****Client Disconnected*****\n");
+		server_delete_client(client_socket);          // Function call for server delete the client
 	}
     else 
     {
