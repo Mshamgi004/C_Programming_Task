911eff1fddbb7bb37181abc5d0377037a9833f6d
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-08-02 01:25:41 +0530

    This file is under working stage as the contents of connected client are showing in text file is showing.

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/client_handle_list.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/client_handle_list.c
@@ -1,99 +1,126 @@
-#include "server.h"
+#inlcude "server.h"
 
-// Function call to handle the text file in server side to read the contents of client
-/************************************************WORKING****************************************/
-void client_handle_list(char *client_buffer, char *ip)
-{	
+// Function to display the contents in the text file
+// *********************************************************WORKING**********************************************************//
+
+
+void client_handle_list(char* client_buffer, char* ip, int port)
+{
 	int val_read;
-   	FILE *fptr;
-   	fptr = fopen("CLIENT_INFO.txt", "w");
-   	if (fptr == NULL) 
+	FILE* fptr;
+	fptr = fopen("CLIENT_INFO.txt", "a+");
+	if (fptr == NULL)
 	{
-    	fprintf(stderr, "\nError to open the file\n");
-      	exit (1);
-   	}
+		fprintf(stderr, "\nError to open the file\n");
+		exit(1);
+	}
 	else
 	{
-		printf("FILE OPENED SUCCESSUFULLY\n");
-	} 
-	
+		printf("FILE OPENED SUCCESSFULLY\n");
+	}
+
 	//fwrite (&detail, sizeof(struct client), 1, fptr);
-	if(strlen(client_buffer) > 0)
+	if (strlen(client_buffer) > 0)
 	{
 		// writing in the file using fputs()
-		fputs(client_buffer, fptr) ;
-		fputs("\n", fptr) ;
+		fputs(client_buffer, fptr);
+		fputs("\n", fptr);
 	}
 	else
 	{
 		printf("Not able to print the name\n");
 	}
 
-	if(strlen(ip) > 0)
+	if (strlen(ip) > 0)
 	{
 		// writing in the file using fputs()
-		fputs(ip, fptr) ;
-		fputs("\n", fptr) ;
+		fputs(ip, fptr);
+		fputs("\n", fptr);
 	}
 	else
 	{
 		printf("Not able to print the ip address\n");
 	}
-	// if(strlen(port) > 0)
+	// if(getw(port) > 0)
 	// {
 	// 	// writing in the file using fputs()
-	// 	fputs(port, fptr) ;
-	// 	fputs("\n", fptr) ;
+	// 	putw(port, fptr) ;
+	// 	putw("\n", fptr) ;
 	// }
 	// else
 	// {
 	// 	printf("Not able to print the port no\n");
 	// }
-	
-	
-	
-   	// if(fwrite != 0)
+
+
+
+	// if(fwrite != 0)
 	// {
 	// 	printf("Contents to file written successfully !\n");
 	// }
-    // else
+	// else
 	// {
-    // 	printf("Error writing file !\n");
+	// 	printf("Error writing file !\n");
 	// }	
-   	fclose(fptr);
-   	
-	fptr = fopen ("CLIENT_INFO.txt", "r");
-   	if (fptr == NULL) 
-	{
-      	fprintf(stderr, "\nError to open the file\n");
-      	exit(1);
-   	}
-	
-	for(val_read = 0; val_read < server.total_client; val_read++)
-	{
-		printf("Started reading\n");
-		//while(fread(&detail, sizeof(struct client), 1, fptr1))
-		while(fgets(client_buffer,MAX_BUFFER_SIZE,fptr) != NULL)
-		{
-    	// printf("NAME = %s\n", detail.client_name);
-		// printf("IP Address = %s\n", detail.ip);
-		// printf("Port = %d\n", detail.port);
-			printf("NAME = %s\n",client_buffer);
-		}
+	// fclose(fptr);
 
-		while(fgets(ip,INET_ADDRSTRLEN,fptr) != NULL)
-		{
-    		// printf("NAME = %s\n", detail.client_name);
-			printf("IP Address = %s\n", ip);
-			// printf("Port = %d\n", detail.port);
-		
-		}
+	// fptr = fopen("CLIENT_INFO.txt", "r");
+	// if (fptr == NULL)
+	// {
+	// 	fprintf(stderr, "\nError to open the file\n");
+	// 	exit(1);
+	// }
+
+	// for (val_read = 0; val_read < server.total_client; val_read++)
+	// {
+	// 	printf("Started reading\n");
+	// 	//while(fread(&detail, sizeof(struct client), 1, fptr1))
+	// 	while (fgets(client_buffer, MAX_BUFFER_SIZE, fptr) != NULL)
+	// 	{
+	// 		// printf("NAME = %s\n", detail.client_name);
+	// 		// printf("IP Address = %s\n", detail.ip);
+	// 		// printf("Port = %d\n", detail.port);
+	// 		printf("NAME = %s\n", client_buffer);
+	// 	}
+
+	// 	while (fgets(ip, INET_ADDRSTRLEN, fptr) != NULL)
+	// 	{
+	// 		// printf("NAME = %s\n", detail.client_name);
+	// 		printf("IP Address = %s\n", ip);
+	// 		// printf("Port = %d\n", detail.port);
+
+	// 	}
+	// }
+
+	// FILE* ptr;
+    // char str[50];
+    // ptr = fopen("test.txt", "a+");
+ 
+    // if (NULL == ptr) {
+    //     printf("file can't be opened \n");
+    // }
+ 
+    printf("-----CONTENT IN THE FILE ARE-------\n");
+ 
+    while (fgets(client_buffer,MAX_BUFFER_SIZE, fptr) != NULL) 
+	{
+        printf("NAME = %s\n", client_buffer);
+    }
+	while(fgets(ip,INET_ADDRSTRLEN,fptr) != NULL)
+	{
+		printf("IP ADDRESS = %s\n", ip);
 	}
 
-  	// //while(fread(&detail, sizeof(struct client), 1, fptr1))
+	// while (getw(port) != NULL)
+	// {
+	// 	printf("Port no. %d\n",port);
+	// }
+
+	fclose(fptr);
+	// //while(fread(&detail, sizeof(struct client), 1, fptr1))
 	// while(fgets(client_buffer,MAX_BUFFER_SIZE,fptr) != NULL)
 	// {
-    // 	// printf("NAME = %s\n", detail.client_name);
+	// 	// printf("NAME = %s\n", detail.client_name);
 	// 	// printf("IP Address = %s\n", detail.ip);
 	// 	// printf("Port = %d\n", detail.port);
 	// 	printf("NAME = %s\n",client_buffer);
@@ -101,18 +128,18 @@ void client_handle_list(char *client_buffer, char *ip)
 
 	// while(fgets(ip,INET_ADDRSTRLEN,fptr) != NULL)
 	// {
-    // 	// printf("NAME = %s\n", detail.client_name);
+	// 	// printf("NAME = %s\n", detail.client_name);
 	// 	printf("IP Address = %s\n", ip);
 	// 	// printf("Port = %d\n", detail.port);
-		
+
 	// }
-	
-	
+
+
 
 	// while(fgets(port,MAX_BUFFER_SIZE,fptr) != NULL)
 	// {
-    // 	printf("Port = %s\n", port);
-  	// }
-   	fclose (fptr);
+	// 	printf("Port = %s\n", port);
+	// }
+	//fclose (fptr);
 }
 
