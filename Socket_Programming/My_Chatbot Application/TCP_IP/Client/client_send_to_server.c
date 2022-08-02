191500d40c122fb2623c10ee879d5fe41c65e72e
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-08-02 17:55:17 +0530

    Used the user end input to get the name of client.

--- Socket_Programming/My_Chatbot Application/TCP_IP/Client/client_send_to_server.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Client/client_send_to_server.c
@@ -1,20 +1,22 @@
 #include "client.h"
 
 // Function defination of sending message to the server
-int client_send_to_server(int client_socket, char *send_msg)
-{	
-	int write_bytes = 0;      // Variable used for writing the message
-	int clilen;         // Declaring a socklen_t variable -> serlen
-	clilen = strlen(send_msg);     // Storing the send buffer in serlen
-		
-	if((write_bytes = send(client_socket, send_msg, serlen,0)) < 0)  // send() message to server 
-	{				
-		printf("****Error: send() failed****\n");
-		exit(1);
+int client_send_to_server(int client_socket, char *send_msg) 
+{
+	int write_bytes = 0;   // Variable used for writing the message
+	int len = strlen(send_msg);    // Declaring a socklen_t variable -> serlen
+
+	printf("Tell your name:%s\n", client_name);
+	//scanf("%s\n", client_name);
+
+	if((write_bytes = send(client_socket, send_msg, len, 0) <= 0))   // Storing the send buffer in serlen
+	{
+		printf("send() failed");
+		return -1;
 	}
 	else
 	{
-		printf("Messgae to server send sucessfully\n");
+		printf("send() passed");
 	}
-	return write_bytes;						
-}	
+	return write_bytes;
+}
