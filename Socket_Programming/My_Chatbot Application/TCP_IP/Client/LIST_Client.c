b88b96281c1b10f035906d72dd5f903ba2154d22
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-08-04 01:39:11 +0530

    Builded for listing the clients connected

---
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Client/LIST_Client.c
@@ -0,0 +1,30 @@
+#include "client.h"
+
+// // To list all client details
+int LIST_clients(char* buffer, char* send_buffer)
+{
+	int index_sender = 0;    // Variable for index_sender verifier
+	int index_receiver = 0;  // Variable for index_reciever verifier
+	int len = 0;
+	//char buffer[MAX_BUFFER_SIZE];
+	int socket;
+
+	index_sender = find_the_client_index_list(socket);
+	if (strncmp(buffer, "LIST", 4) == 0) // Providing the list to client by server using cmd 
+	{
+		// Setting the buffer initially
+		memset(buffer, 0, sizeof(send_buffer));
+
+		// Creating a loop to iterate over a sending messageG
+		for (int val_send = 0; val_send < server.total_client; val_send++)
+		{
+
+			// Concaneting the buffer value to client name's 
+			strcat(buffer, server.client_list[val_send].client_name);
+			strcat(buffer, "&");
+		}
+		//server_send_to_client(socket, buffer);   // sending it to client
+		client_send_to_server(socket, buffer);
+	}
+	return 0;
+}
