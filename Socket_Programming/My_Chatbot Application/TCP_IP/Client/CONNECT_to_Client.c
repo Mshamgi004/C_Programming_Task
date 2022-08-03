ac95b1b86462fb5d517ac2789031a1bc292bd219
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-08-04 01:38:03 +0530

    Builded the function to communicate with other clients

---
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Client/CONNECT_to_Client.c
@@ -0,0 +1,33 @@
+#include "client.h"
+
+// Function defination to communicate with all clients
+int CONNECT_to_Client(char* buffer, char* chat_c)
+{
+	int index_sender = 0;    // Variable for index_sender verifier
+	int index_receiver = 0;  // Variable for index_reciever verifier
+	int len = 0;
+	char send_buffer[MAX_BUFFER_SIZE];
+
+	if (strncmp(buffer, "CONNECT", 7) == 0)     // To connect to another client
+	{
+		sscanf(buffer, "%*[^:]:%s", chat_c);
+		strcpy(server.client_list[index_sender].chatwith, chat_c);
+
+		index_receiver = find_the_client_index_by_name(server.client_list[index_sender].chatwith);
+		server.client_list[index_sender].chatwith_fd = server.client_list[index_receiver].file_des;
+		//server_send_to_client(server.client_list[index_sender].file_des, CONNECTED);
+		client_send_to_server(server.client_list[index_sender].file_des, CONNECTED);
+
+	}
+
+	if (strlen(server.client_list[index_sender].chatwith) != 0)
+	{
+		snprintf(send_buffer, sizeof(send_buffer), "[%s] : %s", server.client_list[index_sender].client_name, buffer);
+		printf("-----------------------\n");
+		printf("Buffer = %s\n", send_buffer);
+
+		//server_send_to_client(server.client_list[index_sender].chatwith_fd, send_buffer);
+		client_send_to_server(server.client_list[index_sender].chatwith_fd, send_buffer);
+	}
+	return 0;
+}
