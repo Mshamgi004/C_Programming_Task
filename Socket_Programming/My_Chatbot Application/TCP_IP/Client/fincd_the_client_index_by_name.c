50d1a0e02cde1870cf59a27e28385e0d7fd8114f
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-08-04 01:40:52 +0530

    Create fincd_the_client_index_by_name.c

---
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Client/fincd_the_client_index_by_name.c
@@ -0,0 +1,15 @@
+#include "client.h"
+
+//find index of the client data structure from client name
+int find_the_client_index_by_name(char* name)
+{
+	int index = 0;
+	for (int index_val1 = 0; index_val1 < server.total_client; index_val1++)
+	{
+		if (strcmp(server.client_list[index_val1].client_name, name) == 0)
+		{
+			index = index_val1;
+		}
+	}
+	return index;
+}
