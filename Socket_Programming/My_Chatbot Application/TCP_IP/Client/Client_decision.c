a5a3298d4048f3536568b29400338596bc45234c
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-08-04 01:33:43 +0530

    User friendly application for a client builded

--- Socket_Programming/My_Chatbot Application/TCP_IP/Client/Client_decision.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Client/Client_decision.c
@@ -1,43 +1,48 @@
-//***************This is the promte messag to be shown on client side so as to perform different operation*******************/
-//----------------------------------------------------WORKING--------------------------------------------------//
 
-
+// Updated the version of function 
 #include "client.h"
 
-int Client_decision(int client_sockfd, char *send_buffer)
+// Fucntion defination in CLient side for a user friendly application
+int Client_decision(int client_sockfd, char* send_buffer)
 {
 	int choice;
 	int option;
-	int client_sockfd;
 	char recv_msg[MAX_BUFFER_SIZE];
-
+	char buffer[MAX_BUFFER_SIZE];
+	char chat_c[MAX_BUFFER_SIZE];
+	int client_socket;
+	int socket_fd_delete;
+	
+	printf("---------------------------------------------------------------------------------\n");
 	printf("Press 1 to know the other client details\n");
-    printf("Press 2 to chat with a client\n");
-    printf("Press 3 to exit\n");
-    printf("Enter your choice:\n");
-    choice = process_recv_data(socket, buffer);  
-      
-    switch (choice) 
+	printf("Press 2 to chat with a client\n");
+	printf("Press 3 to exit\n");
+	printf("Enter your choice:\n");
+	printf("---------------------------------------------------------------------------------\n");
+	scanf("%d", &choice);
+
+	switch (choice)
 	{
-    	case 1: 
+		case 1:
 		{
-        	printf("LIST all client details\n");
-			choice = LIST_clients(buffer,send_buffer);
-        	break;
-    	}
-    	case 2: 
+			printf("LIST all client details\n");
+			LIST_clients(buffer, send_buffer);
+			break;
+		}
+		case 2:
 		{
 			printf("Enter name of client to chat with:\n");
-			choice = CONNECT_to_Client(buffer, chat_c);
+			CONNECT_to_Client(buffer, chat_c);
 			break;
-    	}
-    	case 3:
+		}
+		case 3:
 		{
 			printf("Exit the application\n");
-        	break;
+			server_delete_client(socket_fd_delete);
+			break;
 		}
-    	default:
-        	printf("wrong Input\n");
-    }
-	return 0;
+		// default:
+		// printf("wrong Input\n");
+	}
+		return 0;
 }
