e5059c0e57e5591fd289915a00ab239711f155f0
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-08-04 18:02:18 +0530

    Working as needs modification for user input

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/process_recv_data.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/process_recv_data.c
@@ -17,23 +17,9 @@ int process_recv_data(int socket, char* buffer)
 	memset(buffer, 0, sizeof(send_buffer));
 	scanf("%s\n", send_buffer);
 	
-	for (int val_send = 0; val_send < server.total_client; val_send++)
-	{
-		strcat(buffer, server.client_list[val_send].client_name);
-	}
-	server_send_to_client(socket, buffer);   // sending it to client
-	goto out;
-
-	// if (strncmp(buffer, "LIST", 4) == 0) // Providing the list to client by server using cmd 
-	// {
-	// 	for (int val_send = 0; val_send < server.total_client; val_send++)
-	// 	{
-	// 		strcat(buffer, server.client_list[val_send].client_name);
-	// 	}
-	// 	server_send_to_client(socket, buffer);   // sending it to client
-	// }
 	/*****************UNDER OBSERVETION***********************/
-	/*
+	// Needs modifictaion in terms of user input
+	
 	if (strncmp(buffer, "LIST", 4) == 0) // Providing the list to client by server using cmd 
 	{
 		// Setting the buffer initially
@@ -49,30 +35,27 @@ int process_recv_data(int socket, char* buffer)
 		server_send_to_client(socket, buffer);   // sending it to client
 		goto out;
 	}
-	// else
-	// {
-	// 	printf("****Can't list the items*****\n");
-	// }
-*/
-	// if (strncmp(buffer, "CONNECT", 7) == 0)     // To connect to another client
-	// {
-	// 	sscanf(buffer, "%*[^:]:%s", chat_c);
-	// 	strcpy(server.client_list[index_sender].chatwith, chat_c);
+	
 
-	// 	index_receiver = find_the_client_index_by_name(server.client_list[index_sender].chatwith);
-	// 	server.client_list[index_sender].chatwith_fd = server.client_list[index_receiver].file_des;
-	// 	server_send_to_client(server.client_list[index_sender].file_des, CONNECTED);
-	// 	goto out;
-	// }
+	if (strncmp(buffer, "CONNECT", 7) == 0)     // To connect to another client
+	{
+		sscanf(buffer, "%*[^:]:%s", chat_c);
+		strcpy(server.client_list[index_sender].chatwith, chat_c);
 
-	// if (strlen(server.client_list[index_sender].chatwith) != 0)
-	// {
-	// 	snprintf(send_buffer, sizeof(send_buffer), "[%s] : %s", server.client_list[index_sender].client_name, buffer);
-	// 	printf("-----------------------\n");
-	// 	printf("Buffer = %s\n", send_buffer);
+		index_receiver = find_the_client_index_by_name(server.client_list[index_sender].chatwith);
+		server.client_list[index_sender].chatwith_fd = server.client_list[index_receiver].file_des;
+		server_send_to_client(server.client_list[index_sender].file_des, CONNECTED);
+		goto out;
+	}
 
-	// 	server_send_to_client(server.client_list[index_sender].chatwith_fd, send_buffer);
-	// }
+	if (strlen(server.client_list[index_sender].chatwith) != 0)
+	{
+		snprintf(send_buffer, sizeof(send_buffer), "[%s] : %s", server.client_list[index_sender].client_name, buffer);
+		printf("-----------------------\n");
+		printf("Buffer = %s\n", send_buffer);
+
+		server_send_to_client(server.client_list[index_sender].chatwith_fd, send_buffer);
+	}
 
 out:
 	
