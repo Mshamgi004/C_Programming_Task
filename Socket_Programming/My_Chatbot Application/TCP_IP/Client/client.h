847193b7fb848920b6fef227e56d3fe18d3a08be
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-08-04 18:14:31 +0530

    Update client.h

--- Socket_Programming/My_Chatbot Application/TCP_IP/Client/client.h
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Client/client.h
@@ -17,6 +17,8 @@
 #define SERVER_PORT 9000
 #define MAX_NAME_SZE 20
 #define MAX_BUFFER_SIZE 1024
+#define NO_OF_CLIENTS 3
+#define CONNECTED "Client already Connected...."
 
 char client_name[MAX_NAME_SZE];   // Char buffer used to store the client name
 
@@ -26,3 +28,27 @@ int client_recv_from_server(int socket_client, char *recv_msg);
 int client_send_to_server(int socket_client, char *send_msg);
 int client_build_fdsets(int listenfd, fd_set *readfds, fd_set *writefds, fd_set *exceptfds);
 int client_select(int maxval_fd, int listenfd, fd_set *readfds, fd_set *writefds);
+int LIST_clients(char* buffer, char* send_buffer);
+int CONNECT_to_Client(char* buffer, char* chat_c);
+int find_the_client_index_list(int socket);
+int find_the_client_index_by_name(char* name);
+int Client_decision(int client_sockfd, char* send_buffer);
+void server_delete_client(int socket_fd_delete); 
+
+// Structure to hold the name, ip-address, port and file des 
+struct client 
+{
+	char client_name[MAX_NAME_SZE];
+	char chatwith[MAX_NAME_SZE];
+	int chatwith_fd;
+	int file_des;
+	int port;
+	char ip[INET_ADDRSTRLEN];
+};
+
+// Data structure to hold the total no of clients
+struct server_data 
+{
+	int total_client;
+	struct client client_list[NO_OF_CLIENTS];
+}server;
