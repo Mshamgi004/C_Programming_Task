1a880d6520ecf7016addc28b526fa02a59cc1f7f
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-08-08 18:08:10 +0530

    Working on the modification of file handling operation.

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/client_handle_list.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/client_handle_list.c
@@ -1,10 +1,10 @@
-#include "server.h"
-
 void client_handle_list(char* client_buffer, char* ip, char* port_number)
 {
-	int val_read;
+	int wordExist = 0;
+	char search[MAX_BUFFER_SIZE];
+
 	FILE* fptr;
-	fptr = fopen("CLIENT_INFO.txt", "a+");
+	fptr = fopen("CLIENT_INFO.txt", "w");
 	if (fptr == NULL)
 	{
 		fprintf(stderr, "\nError to open the file\n");
@@ -27,40 +27,118 @@ void client_handle_list(char* client_buffer, char* ip, char* port_number)
 		printf("Not able to print the name\n");
 	}
 
-	if (strlen(ip) > 0)
-	{
-		// writing in the file using fputs()
-		fputs(ip, fptr);
-		fputs("\n", fptr);
-	}
-	else
-	{
-		printf("Not able to print the ip address\n");
-	}
-	if(strlen(port_number) > 0)
-	{
-		// writing in the file using fputs()
-		fputs(port_number, fptr) ;
-		fputs("\n", fptr) ;
-	}
-	else
-	{
-		printf("Not able to print the port no\n");
-	}
-	
- 	while (fgets(client_buffer,MAX_BUFFER_SIZE, fptr) != NULL) 
-	{
-        printf("NAME = %s\n", client_buffer);
-    }
-	while(fgets(ip,INET_ADDRSTRLEN,fptr) != NULL)
-	{
-		printf("IP ADDRESS = %s\n", ip);
-	}
-	while(fgets(port_number,MAX_BUFFER_SIZE,fptr) != NULL)
-	{
-		printf("Port_number = %s\n", port_number);
-	}
+	// if (strlen(ip) > 0)
+	// {
+	// 	// writing in the file using fputs()
+	// 	fputs(ip, fptr);
+	// 	fputs("\n", fptr);
+	// }
+	// else
+	// {
+	// 	printf("Not able to print the ip address\n");
+	// }
+	// if(strlen(port_number) > 0)
+	// {
+	// 	// writing in the file using fputs()
+	// 	fputs(port_number, fptr) ;
+	// 	fputs("\n", fptr) ;
+	// }
+	// else
+	// {
+	// 	printf("Not able to print the port no\n");
+	// }
+	fclose(fptr);
 
+	fptr = fopen("CLIENT_INFO.txt", "r");
+	if(fptr == NULL)
+	{
+		fprintf(stderr,"\nError to open the file\n");
+		exit(1);
+	}
+	else
+	{
+		printf("Contents in file are being readed\n");
+	}
 	
+	/////////////////////////////////////////////BUILDING THE LOGIC : WORKING ///////////////////////////////////////////////
+	//printf("NAME = %s\n", client_buffer);
+	
+	
+	while (fscanf(fptr,"%s", client_buffer) != EOF) 
+	{
+        //printf("NAME = %s\n", client_buffer);
+
+		printf("Comparing the names\n");
+		if(strcmp(client_buffer,search) == 0)
+		{
+			// wordExist = 1;
+			// break;
+			printf("Client name stored\n");
+		}
+		// fclose(fptr);
+
+		// if(wordExist == 1)
+		// {	
+		// 	printf("HIUHGES\n");
+		// 	printf("Client name already registered\n");
+		// }
+		else
+		{
+			fptr = fopen("CLIENT_INFO.txt", "w");
+			if(fptr == NULL)
+			{
+				fprintf(stderr, "\nError to open the file\n");
+			}
+			else
+			{
+				printf("Name added successfully\n");
+			}
+
+			if (strlen(client_buffer) > 0)
+			{
+				// writing in the file using fputs()
+				fputs(client_buffer, fptr);
+				fputs("\n", fptr);
+			}
+			else
+			{
+				printf("Not able to print the name\n");
+			}
+			printf("NEW CLIENT ADDED\n");
+
+		}
+    }
+	// while(fgets(ip,INET_ADDRSTRLEN,fptr) != NULL)
+	// {
+	// 	printf("IP ADDRESS = %s\n", ip);
+	// }
+	// while(fgets(port_number,MAX_BUFFER_SIZE,fptr))
+	// {
+	// 	printf("Comparing the strings\n");
+	// 	if(strstr(port_number,search) != NULL)
+	// 	{
+	// 		wordExist = 1;
+	// 		break;
+	// 	}
+	// 	// fclose(fptr);
+
+	// 	if(wordExist == 1)
+	// 	{
+	// 		printf("HIIIIIII");
+	// 		printf("Client port no: already registered\n");
+	// 	}
+	// 	else
+	// 	{
+	// 		fptr = fopen("CLIENT_INFO.txt", "a+");
+	// 		if(fptr == NULL)
+	// 		{
+	// 			fprintf(stderr, "\nError to open the file\n");
+	// 		}
+	// 		else
+	// 		{
+	// 			printf("Port no added successfully\n");
+	// 		}
+	// 	}
+    //}
 	fclose (fptr);
 }
