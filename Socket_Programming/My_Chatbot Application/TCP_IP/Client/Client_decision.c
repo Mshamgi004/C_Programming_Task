4eeb6ec1fad0ba5b8bc0ecb2997198df3c8924d1
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-08-12 13:18:02 +0530

    Updated the working style of menu driven part.

--- Socket_Programming/My_Chatbot Application/TCP_IP/Client/Client_decision.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Client/Client_decision.c
@@ -3,11 +3,11 @@
 
 int Client_decision(int client_sockfd, char* send_buffer)
 {
-	char choice;
-	int select = 1;
-	char input[MAX_BUFFER_SIZE];
-	int option;
-	int input_status;
+	//char choice;
+	//int select = 1;
+	//char input[MAX_BUFFER_SIZE];
+	//int option;
+	//int input_status;
 	char recv_msg[MAX_BUFFER_SIZE];
 	char buffer[MAX_BUFFER_SIZE];
 	char chat_c[MAX_BUFFER_SIZE];
@@ -16,29 +16,32 @@ int Client_decision(int client_sockfd, char* send_buffer)
 	
 	// ---------------------------------WORKING ------------------------------------------------//
 	// Trying to build the exact operation for menu driven function in client side
-	while(select == 1)
-	{
-		printf("---------------------------------------------------------------------------------\n");
-		printf("Press 1 to know the other client details\n");
-		printf("Press 2 to chat with a client\n");
-		printf("Press 3 to exit\n");
-		printf("---------------------------------------------------------------------------------\n");
-		printf("Enter your choice:");
-		scanf("%s",input);
+// 	while(select == 1)
+// 	{
+		printf("_________________WELCOME TO MY_CHATBOT_APPLICATION_______________\n");
+		printf("\n\
+		1. 	Press 1 to know the other client details\n\
+		2. 	Press 2 to chat with a client\n\
+		3.	Press 3 to exit\n");
+		
+		printf("\nEnter your choice -> ");
+		printf("________________________\n");
+		scanf("%d", &choice);
 	
-		input_status = input;         // Checking for the condition to be true for input status
-		if(input_status == 1)
-		{
-			continue;
-        }
-		else      // if input is a digit
-        {
-            option = input_status;
-			if(option == 0)
-			{
-				return -1;
-			}
-		}
+	// Earlier was trying to build a cgecking condition.
+// 		input_status = input;         // Checking for the condition to be true for input status
+// 		if(input_status == 1)
+// 		{
+// 			continue;
+//         }
+// 		else      // if input is a digit
+//         {
+//             option = input_status;
+// 			if(option == 0)
+// 			{
+// 				return -1;
+// 			}
+// 		}
 		switch (option)
 		{
 			case 1:
@@ -63,6 +66,7 @@ int Client_decision(int client_sockfd, char* send_buffer)
 				printf("Invalid Option\n Try Again\n");
 				break;
 		}
-	}	
+	//}	
+	getchar();
 	return 0;
 }
