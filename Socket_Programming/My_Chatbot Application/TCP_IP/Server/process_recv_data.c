37cd22a71797dddee8538313882a0701ee25fbb8
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-08-17 17:44:34 +0530

    Updating the wokflow without cmd

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/process_recv_data.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/process_recv_data.c
@@ -14,39 +14,39 @@ int process_recv_data(int socket, char* buffer)
 
 	index_sender = find_the_client_index_list(socket); // Storing the index values of client in index_sender
 
-	memset(buffer, 0, sizeof(send_buffer));
-	scanf("%s\n", send_buffer);
 	
-	/*****************UNDER OBSERVETION***********************/
-	// Needs modifictaion in terms of user input
-	
-	if (strncmp(buffer, "LIST", 4) == 0) // Providing the list to client by server using cmd 
-	{
+	//if (strncmp(buffer, "LIST", 4) == 0) // Providing the list to client by server using cmd 
+	//{
 		// Setting the buffer initially
-		memset(buffer, 0, sizeof(send_buffer));
+	memset(buffer, 0, sizeof(send_buffer));
 
 		// Creating a loop to iterate over a sending message
-		for (int val_send = 0; val_send < server.total_client; val_send++)
-		{
-			// Concaneting the buffer value to client name's 
-			strcat(buffer, server.client_list[val_send].client_name);
-			strcat(buffer, "&");
-		}
-		server_send_to_client(socket, buffer);   // sending it to client
-		goto out;
-	}
-	
-
-	if (strncmp(buffer, "CONNECT", 7) == 0)     // To connect to another client
+	for (int val_send = 0; val_send < server.total_client; val_send++)
 	{
-		sscanf(buffer, "%*[^:]:%s", chat_c);
+			// Concaneting the buffer value to client name's 
+		printf("------Names of clients conneted--------\n");
+		strcat(buffer, server.client_list[val_send].client_name);
+		strcat(buffer, "\n");
+	}
+	server_send_to_client(socket, buffer);   // sending it to client
+	//goto out;
+//}
+	// else
+	// {
+	// 	printf("****Can't list the items*****\n");
+	// }
+
+	//if (strncmp(buffer, "CONNECT", 7) == 0)     // To connect to another client
+	//{
+		printf("---I want to talk with %s----", buffer);
+		scanf(buffer, "%s", chat_c);
 		strcpy(server.client_list[index_sender].chatwith, chat_c);
 
 		index_receiver = find_the_client_index_by_name(server.client_list[index_sender].chatwith);
 		server.client_list[index_sender].chatwith_fd = server.client_list[index_receiver].file_des;
 		server_send_to_client(server.client_list[index_sender].file_des, CONNECTED);
-		goto out;
-	}
+		//goto out;
+	//}
 
 	if (strlen(server.client_list[index_sender].chatwith) != 0)
 	{
@@ -57,7 +57,7 @@ int process_recv_data(int socket, char* buffer)
 		server_send_to_client(server.client_list[index_sender].chatwith_fd, send_buffer);
 	}
 
-out:
+//out:
 	
 	return 0;
 }
