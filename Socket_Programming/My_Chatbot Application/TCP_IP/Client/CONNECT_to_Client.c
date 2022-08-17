4fc72b0891d48beec0ea8bdb39cf68f0fce611ae
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-08-17 17:57:06 +0530

    Changed the use of receiving the message from user.

--- Socket_Programming/My_Chatbot Application/TCP_IP/Client/CONNECT_to_Client.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Client/CONNECT_to_Client.c
@@ -1,24 +1,24 @@
+// Function defination to communicate with all clients
+// Working on cmd operatio so as to remove the command line user input and send the message form user input only ....... 
+
 #include "client.h"
 
-// Function defination to communicate with all clients
-int CONNECT_to_Client(char* buffer, char* chat_c)
+int CONNECT_to_Client(char *buffer, char *chat_c)
 {
-	int index_sender = 0;    // Variable for index_sender verifier
-	int index_receiver = 0;  // Variable for index_reciever verifier
+	int index_sender = 0;	// Variable for index_sender verifier
+	int index_receiver = 0; // Variable for index_reciever verifier
 	int len = 0;
 	char send_buffer[MAX_BUFFER_SIZE];
 
-	if (strncmp(buffer, "CONNECT", 7) == 0)     // To connect to another client
-	{
-		sscanf(buffer, "%*[^:]:%s", chat_c);
-		strcpy(server.client_list[index_sender].chatwith, chat_c);
+	// if (strncmp(buffer, "CONNECT", 7) == 0)     // To connect to another client
+	//{
+	scanf(buffer, "%s", chat_c);
+	strcpy(server.client_list[index_sender].chatwith, chat_c);
 
-		index_receiver = find_the_client_index_by_name(server.client_list[index_sender].chatwith);
-		server.client_list[index_sender].chatwith_fd = server.client_list[index_receiver].file_des;
-		//server_send_to_client(server.client_list[index_sender].file_des, CONNECTED);
-		client_send_to_server(server.client_list[index_sender].file_des, CONNECTED);
-
-	}
+	index_receiver = find_the_client_index_by_name(server.client_list[index_sender].chatwith);
+	server.client_list[index_sender].chatwith_fd = server.client_list[index_receiver].file_des;
+	// server_send_to_client(server.client_list[index_sender].file_des, CONNECTED);
+	client_send_to_server(server.client_list[index_sender].file_des, CONNECTED);
 
 	if (strlen(server.client_list[index_sender].chatwith) != 0)
 	{
@@ -26,7 +26,7 @@ int CONNECT_to_Client(char* buffer, char* chat_c)
 		printf("-----------------------\n");
 		printf("Buffer = %s\n", send_buffer);
 
-		//server_send_to_client(server.client_list[index_sender].chatwith_fd, send_buffer);
+		// server_send_to_client(server.client_list[index_sender].chatwith_fd, send_buffer);
 		client_send_to_server(server.client_list[index_sender].chatwith_fd, send_buffer);
 	}
 	return 0;
