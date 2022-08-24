729bb485447adea2395607692f6f7c014affc8d1
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-08-24 18:27:02 +0530

    Update client_recv_from_server.c

--- Socket_Programming/My_Chatbot Application/TCP_IP/Client/client_recv_from_server.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Client/client_recv_from_server.c
@@ -1,22 +1,26 @@
 #include "client.h"
 
 // Function defination to recv message from server
-int client_recv_from_server(int client_socket, char *recv_msg)
+int client_recv_from_server(int client_socket, char* recv_msg)
 {
-		int read_bytes = 0;      // Variable use to store the read_bytes 
-		
-		if((read_bytes = recv(client_socket, recv_msg, MAX_BUFFER_SIZE, 0)) > 0)   // recv() message from server
-		{
-			printf("%s\n",recv_msg);
-		}	  
-		else if(read_bytes == 0)
-		{
-			printf("*****Client Disconnected*****\n");
-			close(client_socket);
-		}
-		else
-		{
-			printf("*****ERROR: recv() failes******\n");
-		}
-		return 0;
+	int read_bytes = 0;    // Variable use to store the read_bytes 
+	int socket_fd_delete;
+
+	if ((read_bytes = recv(client_socket, recv_msg, MAX_BUFFER_SIZE, 0)) > 0)   // recv() message from server
+	{
+		printf("%s\n", recv_msg);
+
+	}
+	else if (read_bytes == 0)
+	{
+		printf("*****Client Disconnected*****\n");
+		server_delete_client(socket_fd_delete);
+		close(client_socket);
+	}
+	else
+	{
+		printf("*****ERROR: recv() failed*****\n");
+	}
+
+	return 0;
 }
