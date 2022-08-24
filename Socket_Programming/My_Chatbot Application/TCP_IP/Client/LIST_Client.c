51122a919c346de1750f6f2849b426e0e433c894
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-08-24 18:26:22 +0530

    Update LIST_Client.c

--- Socket_Programming/My_Chatbot Application/TCP_IP/Client/LIST_Client.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Client/LIST_Client.c
@@ -1,7 +1,6 @@
 #include "client.h"
 
 //  To list all client details
-// Working on the removing part of cmd operation so as to take the user input
 int LIST_clients(char* buffer, char* send_buffer)
 {
 	int index_sender = 0;    // Variable for index_sender verifier
@@ -11,24 +10,21 @@ int LIST_clients(char* buffer, char* send_buffer)
 	int socket;
 
 	index_sender = find_the_client_index_list(socket);
-	
-	//if (strncmp(buffer, "LIST", 4) == 0) // Providing the list to client by server using cmd 
-	//{
-	printf("List all clients\n");
-	scanf("%s", buffer);
-		// Setting the buffer initially
-	memset(buffer, 0, sizeof(send_buffer));
-
-		// Creating a loop to iterate over a sending messageG
-	for (int val_send = 0; val_send < server.total_client; val_send++)
+	if (strncmp(buffer, "LIST", 4) == 0) // Providing the list to client by server using cmd 
 	{
-		// Concaneting the buffer value to client name's
-		printf("List of clients connected\n"); 
-		strcat(buffer, server.client_list[val_send].client_name);
-		strcat(buffer, "\n");
-	}
+        // Setting the buffer initially
+		memset(buffer, 0, sizeof(send_buffer));
+        
+        // Creating a loop to iterate over a sending messageG
+		for (int val_send = 0; val_send < server.total_client; val_send++)
+		{
+            // Concaneting the buffer value to client name's 
+            printf("List of clients connected\n");
+			strcat(buffer, server.client_list[val_send].client_name);
+			strcat(buffer, "\n");
+		}
 		//server_send_to_client(socket, buffer);   // sending it to client
 		client_send_to_server(socket, buffer);
-	//}
+	}
 	return 0;
 }
