87d44f1c458f52500970b0acddc81d8234d3457c
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-08-24 18:11:34 +0530

    Update Structure_List.h

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/Structure_List.h
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/Structure_List.h
@@ -1,16 +1,21 @@
 // Use of Struture_LIst.h so as to simplify the file operation
 
 // Use of structure 
-struct client 
+struct client_List
 {
 	char client_name[1000];
+	char chatwith[1000];
+	int chatwith_fd;
+	int file_des;
 	int port;
 	char ip[INET_ADDRSTRLEN];
 
 };
 
+/*
 // Link list of structure client to accces the name, ip and port
 struct link_list
 {
-    struct client file;
+    struct client_List file[100];
 };
+*/
