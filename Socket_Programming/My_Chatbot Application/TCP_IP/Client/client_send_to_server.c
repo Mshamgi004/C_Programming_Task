d7e5e07bbd80878976cd2c4f0e74cf2022ade156
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-08-26 10:57:28 +0530

    Working on removing the cmd operation

--- Socket_Programming/My_Chatbot Application/TCP_IP/Client/client_send_to_server.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Client/client_send_to_server.c
@@ -1,12 +1,13 @@
 #include "client.h"
 
+//********************WORKING on removing the cmd operation*******************//
 // Function defination of sending message to the server
 int client_send_to_server(int client_socket, char *send_msg) 
 {
 	int write_bytes = 0;   // Variable used for writing the message
 	int len = strlen(send_msg);    // Declaring a socklen_t variable -> serlen
 
-	printf("Tell your name:%s\n", client_name);
+	//printf("Tell your name:%s\n", client_name);
 	//scanf("%s\n", client_name);
 
 	if((write_bytes = send(client_socket, send_msg, len, 0) <= 0))   // Storing the send buffer in serlen
