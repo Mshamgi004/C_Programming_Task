2da95e5de1cb35586426019d8c338e3c5b5673fd
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-08-29 00:42:40 +0530

    Update in the terminal output of server side

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/server_main.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/server_main.c
@@ -1,38 +1,38 @@
 #include "server.h"
 
-int main() 
-{
-	// Declaring the read ,write and except fd's for select()
+int main()
+{	
+    // fd_set for reading, writing and exception for socket fd
 	fd_set readfds;
 	fd_set writefds;
 	fd_set exceptfds;
-	
-	int server_sockfd;   // Server socket declared
-	
-	int maxval_fd = 0;   // A max sock_fd declared to hold the server_sockfd value
-	
-	memset(&server,0,sizeof(struct server_data));   // Clearing the structure to hold the total clients
-	printf("------------------------------SERVER STARTED----------------------------!!!\n");
-	printf("___________________________________________________________________________\n");
+    int server_sockfd;
 
-	if(setup_server(&server_sockfd) != 0)   // Function call for setting up of server
+	int maxval_fd = 0;   // A max sock_fd declared to hold the server_sockfd value
+
+	memset(&server, 0, sizeof(struct server_data));   // Clearing the structure to hold the total clients
+	printf("\t||---------------------------SERVER STARTED-------------------------------||\n");
+	printf("\t||************************************************************************||\n");
+	
+
+	if (setup_server(&server_sockfd) != 0)   // Function call for setting up of server
 	{
-        printf("*****ERROR : Creation socket failed******");
-        exit(0);
-    }
+		printf("*****ERROR : Creation socket failed******");
+		exit(0);
+	}
 	else
 	{
-    	maxval_fd = server_sockfd;   // Equalising the max_fd to server_fd
+		maxval_fd = server_sockfd;   // Equalising the max_fd to server_fd
 	}
-    
-	while(1) 
-    {
-        maxval_fd = server_build_fdsets(server_sockfd, &readfds, &writefds, &exceptfds);   // server_socket will build the fd_sets and will return the maxval_fd 
-        server_select(maxval_fd,server_sockfd, &readfds, &writefds);     // Function call for select()
-    }
 
-    cleanup();
-    printf("Bye From server\n");
+	while (1)
+	{
+		maxval_fd = server_build_fdsets(server_sockfd, &readfds, &writefds, &exceptfds);   // server_socket will build the fd_sets and will return the maxval_fd 
+		server_select(maxval_fd, server_sockfd, &readfds, &writefds);     // Function call for select()
+	}
 
-    return 0;
+	cleanup();
+	printf("Bye From server\n");
+
+	return 0;
 }
