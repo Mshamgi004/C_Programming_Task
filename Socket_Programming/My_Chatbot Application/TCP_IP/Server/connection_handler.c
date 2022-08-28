f7ddf37dc419123562e9863cf63678c09227c217
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-08-29 00:47:56 +0530

    Update connection_handler.c

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/connection_handler.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/connection_handler.c
@@ -1,42 +1,43 @@
 #include "server.h"
-
 // Function defination to handle the client
 void handle_connection(struct sockaddr_in client_information, int new_server_sockfd)
 {
-    // get the name, IP Address and port no: client
-    char ip[INET_ADDRSTRLEN] = {0};            // char buffer to store the ip address of connected clients
-    char client_buffer[MAX_BUFFER_SIZE] = {0}; // char buffer to store the name of connected clients
-    int port = ntohs(client_information.sin_port);
-    char port_number[MAX_BUFFER_SIZE];
+	//get the name, IP Address and port no: client 
+	char ip[INET_ADDRSTRLEN] = { 0 };  // char buffer to store the ip address of connected clients
+	char client_buffer[MAX_BUFFER_SIZE] = { 0 };   // char buffer to store the name of connected clients
+	int port = ntohs(client_information.sin_port);
+	char port_number[MAX_BUFFER_SIZE];
+	
+	inet_ntop(AF_INET, &(client_information.sin_addr), ip, INET_ADDRSTRLEN);
+	
+	server_recv_from_client(new_server_sockfd, client_buffer);  // Function call to recieve the name from client
 
-    inet_ntop(AF_INET, &(client_information.sin_addr), ip, INET_ADDRSTRLEN);
+	printf("\t||--------------Details of the client connected to server-----------------||\n");
+	printf("\t||------------------------------------------------------------------------||\n");
+	printf("\t|| [CLIENT NAME] : %s\n", client_buffer);
+	printf("\t||------------------------------------------------------------------------||\n");
+	printf("\t|| [IP ADDRESS]  : %s\n", ip);
+	printf("\t||------------------------------------------------------------------------||\n");
+	sprintf(port_number,"%d",port);
+	printf("\t|| [Port NUMBER] : %s\n",port_number); 
+	printf("\t||------------------------------------------------------------------------||\n");
 
-    server_recv_from_client(new_server_sockfd, client_buffer); // Function call to recieve the name from client
 
-    // Updation on reciveing the name and port number on server without cmd
-    printf("_________________________________\n");
-    printf("\n[CLIENT NAME] : %s\n", client_buffer);
-    printf("_________________________________\n");
-    printf("\n[IP ADDRESS]  : %s\n", ip);
-    printf("_________________________________\n");
-    sprintf(port_number, "%d", port);
-    printf("\n[Port NUMBER] : %s\n", port_number);
-    printf("_________________________________\n");
+	printf("\t __________________Storing client data in CLIENT_INFO.txt___________________\n");
 
-    printf("Storing client data in CLIENT_INFO.txt\n");
+	client_handle_list(client_buffer, ip, port_number);
+	// To check for the no. of clients connected 
+	if (server.total_client >= NO_OF_CLIENTS)
+	{
+		printf("****ERROR : No more space for client to save****\n");
+	}
 
-    client_handle_list(client_buffer, ip, port_number);
-    // To check for the no. of clients connected
-    if (server.total_client >= NO_OF_CLIENTS)
-    {
-        printf("****ERROR : No more space for client to save****\n");
-    }
+	//populate the new client data and saving it in client_list
+	strncpy(server.client_list[server.total_client].client_name, client_buffer, strlen(client_buffer));
+	server.client_list[server.total_client].port = port;
+	strcpy(server.client_list[server.total_client].ip, ip);
+	server.client_list[server.total_client].file_des = new_server_sockfd;
 
-    // populate the new client data and saving it in client_list
-    strncpy(server.client_list[server.total_client].client_name, client_buffer, strlen(client_buffer));
-    server.client_list[server.total_client].port = port;
-    strcpy(server.client_list[server.total_client].ip, ip);
-    server.client_list[server.total_client].file_des = new_server_sockfd;
+	server.total_client++;
 
-    server.total_client++;
 }
