b25c647a89d60141a2f6350901b51cb383626fdc
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-08-29 00:55:03 +0530

    Changes in the command prompt of client side

--- Socket_Programming/My_Chatbot Application/TCP_IP/Client/Client_decision.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Client/Client_decision.c
@@ -1,46 +1,44 @@
 #include "client.h"
 
 
-int Client_decision(int client_sockfd, char* send_buffer)
+int Client_decision()//(int client_sockfd, char* send_buffer)
 {
 	int choice;
-	int option;
-	char recv_msg[MAX_BUFFER_SIZE];
-	char buffer[MAX_BUFFER_SIZE];
-	char chat_c[MAX_BUFFER_SIZE];
-	int client_socket;
-	int socket_fd_delete;
+	//char recv_msg[MAX_BUFFER_SIZE];
+	//char buffer[MAX_BUFFER_SIZE];
+	//char chat_c[MAX_BUFFER_SIZE];
+	//int client_socket;
+	//int socket_fd_delete;
 	
-	printf("--------------------------------CLIENT STARTED-----------------------------------\n");
-	printf("---------------------------------------------------------------------------------\n");
+	printf("\t||----You have entered the application. Now choose what you have to do----||\n");
+	printf("\t||    Note: Please select a number between 1 to 3.                        ||\n");
+	printf("\t||************************************************************************||\n");
 	printf("\n\
 	1. 	Press 1 to know the other client details\n\
 	2. 	Press 2 to chat with a client\n\
 	3.	Press 3 to exit\n");
-	printf("Enter your choice:");
-	printf("\n---------------------------------------------------------------------------------\n");
-	
-	
-	scanf("%d", &choice);
+	printf("\nEnter your choice:\n");
+	printf("\n______________\n");
+	scanf("\n%d", &choice);
 
 	switch (choice)
 	{
 		case 1:
 		{
 			printf("LIST all client details\n");
-			LIST_clients(buffer, send_buffer);
+			//LIST_clients(buffer, send_buffer);
 			break;
 		}
 		case 2:
 		{
 			printf("Enter name of client to chat with:\n");
-			CONNECT_to_Client(buffer, chat_c);
+			//CONNECT_to_Client(buffer, chat_c);
 			break;
 		}
 		case 3:
 		{
 			printf("Exit the application\n");
-			server_delete_client(socket_fd_delete);
+			//server_delete_client(socket_fd_delete);
 			break;
 		}
 		// default:
