fbf91007621b371e76d95d85780c8afaec205ac0
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-08-29 00:45:30 +0530

    Update server_delete_client.c

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/server_delete_client.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/server_delete_client.c
@@ -1,20 +1,21 @@
 #include "server.h"
 
 // Function defination to delete the client
-void server_delete_client(int socket_fd_delete) 
+void server_delete_client(int socket_fd_delete)
 {
-    int delete = 0;
-    int index = 0;
+	int delete = 0;
+	int index = 0;
 
 	// Iterating the variable till tital no. of clients
-    for(delete = 0; delete < NO_OF_CLIENTS; delete++) 
+	for (delete = 0; delete < NO_OF_CLIENTS; delete++)
 	{
 		// Condition for deleting the client
-        if(server.client_list[delete].file_des == socket_fd_delete) 
+		if (server.client_list[delete].file_des == socket_fd_delete)
 		{
-            server.total_client--;
-			printf("-------Socket deleted = [%d]\n",socket_fd_delete);
-        }
-    }
-    close(socket_fd_delete);
+			server.total_client--;
+			printf("\t------------------Connected client deleted = [%d]----------------------------\n", socket_fd_delete);
+		}
+	}
+
+	close(socket_fd_delete);
 }
