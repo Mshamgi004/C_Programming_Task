0e763525c6ca85d46b65f46fb227a65848aa353d
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-08-29 00:44:16 +0530

    Update server_send_to_client.c

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/server_send_to_client.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/server_send_to_client.c
@@ -1,20 +1,19 @@
 #include "server.h"
 
 // Function defination to send the message to client
-int server_send_to_client(int client_socket, char *send_msg) 
+int server_send_to_client(int client_socket, char* send_msg)
 {
 	int write_bytes = 0;   // Variable used for writing the message
 	int server_length;   // Creating a variable -> server_length  
-    server_length = strlen(send_msg);   // assigning the address to the variable server_length
-	
-	if((write_bytes = send(client_socket, send_msg, server_length, 0)) > 0)   // send() message
-	{	
-    			
-		printf("\n[CLIENT : %d]",client_socket);
-    }
-    else 
-    {
-		printf("*****Error : send failed*****");
+	server_length = strlen(send_msg);   // assigning the address to the variable server_length
+
+	if ((write_bytes = send(client_socket, send_msg, server_length, 0)) > 0)   // send() message
+	{
+		printf("\t||---------------------------[CLIENT : %d]--------------------------------||\n", client_socket);
+	}
+	else
+	{
+		perror("*****Error : send failed*****");
 		return -1;
 	}
 	return write_bytes;
