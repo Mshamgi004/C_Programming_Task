0859b04110df7625a116c8d752c18838792a6daf
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-08-30 00:07:34 +0530

    Update server_send_to_client.c

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/server_send_to_client.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/server_send_to_client.c
@@ -1,56 +1,20 @@
 #include "server.h"
 
-// Function defination of server_select
-int server_select(int maxval_fd, int server_sockfd, fd_set* readfds, fd_set* writefds)
+// Function defination to send the message to client
+int server_send_to_client(int client_socket, char* send_msg)
 {
-	// Declaring the new server sock_fd and char buffer for storing the sending and recieving message
-	int new_server_sockfd = 0;
-	char recv_buff[MAX_BUFFER_SIZE];
-	char send_buff[MAX_BUFFER_SIZE];
+	int write_bytes = 0;   // Variable used for writing the message
+	int server_length;   // Creating a variable -> server_length  
+	server_length = strlen(send_msg);   // assigning the address to the variable server_length
 
-	// To fill the send and recv buffer with string input
-	memset(recv_buff, 0, sizeof(recv_buff));
-	memset(send_buff, 0, sizeof(send_buff));
-
-	int select_action = select(maxval_fd + 1, readfds, writefds, NULL, NULL);  // select() system call 
-
-	// Checking for the working of select() system call
-	if (select_action == -1 || select_action == 0)
+	if ((write_bytes = send(client_socket, send_msg, server_length, 0)) > 0)   // send() message
 	{
-		printf("*****ERROR : select()*****");
-		exit(0);
+		printf("\t||---------------------------[CLIENT : %d]--------------------------------||\n", client_socket);
 	}
-
-	// To check the fd is set so as to accept the new connection request
-	if (FD_ISSET(server_sockfd, readfds))
+	else
 	{
-		// Then accecpt the new connection
-		accept_new_connection(server_sockfd, &new_server_sockfd);
-		printf("\t||-----------------------New socket created = %d--------------------------||\n", new_server_sockfd);
+		perror("*****Error : send failed*****");
+		return -1;
 	}
-
-	// To check the fd is set for sending the message so as to broadcast the message to all clients
-	if (FD_ISSET(STDIN_FILENO, readfds))
-	{
-		// To check for message to be send
-		if (read(0, send_buff, sizeof(send_buff)) > 0)
-		{
-			// Then fill the send_buff with message
-			for (int val1 = 0; val1 < server.total_client; val1++)
-			{
-				server_send_to_client(server.client_list[val1].file_des, send_buff);
-			}
-		}
-	}
-
-	for (int val2 = 0; val2 < server.total_client; val2++)
-	{
-		// To check for all the clients in the file des inside the readfds
-		if (FD_ISSET(server.client_list[val2].file_des, readfds))
-		{
-			// If its present in readfds then it will recieve the message
-			server_recv_from_client(server.client_list[val2].file_des, recv_buff);
-		}
-	}
-	return 0;
+	return write_bytes;
 }
