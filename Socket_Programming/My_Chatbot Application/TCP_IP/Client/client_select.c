e0dae9c157be00fe93a9d4483fbac72fb90e38ee
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-09-14 13:04:32 +0530

    Update client_select.c

--- Socket_Programming/My_Chatbot Application/TCP_IP/Client/client_select.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Client/client_select.c
@@ -10,25 +10,26 @@ int client_select(int maxval_fd,int client_sockfd, fd_set *readfds, fd_set *writ
 	memset(send_buff, 0 ,sizeof(send_buff));
     
 	int client_action = select(maxval_fd+1,readfds,writefds,NULL,NULL);   // select() system call 
-
+	
+	// To check if select() sys call is working or not
 	if(client_action == -1 || client_action == 0) 
 	{
-        printf("****ERROR: select()****\n");
-        exit(0);
-    }
+		printf("****ERROR: select()****\n");
+		exit(0);
+    	}
 	// To check for all the clients in the file des inside the readfds
-    if(FD_ISSET(client_sockfd,readfds)) 
-    {	
-     	// If its present in readfds then it will recieve the message
-        client_recv_from_server(client_sockfd,recv_msgg);             
-    }
+    	if(FD_ISSET(client_sockfd,readfds)) 
+    	{	
+		// If its present in readfds then it will recieve the message
+		client_recv_from_server(client_sockfd,recv_msgg);             
+    	}
 	// To check the fd is set for sending the message so as to broadcast the message to all clients
-    if(FD_ISSET(STDIN_FILENO,readfds)) 
-    {
+   	if(FD_ISSET(STDIN_FILENO,readfds)) 
+    	{
 		if(read(0,send_buff,sizeof(send_buff)) > 0) 
 		{
-            client_send_to_server(client_sockfd,send_buff);
-        }
-    }
-    return 0;
+            		client_send_to_server(client_sockfd,send_buff);
+        	}
+   	}
+	return 0;
 }
