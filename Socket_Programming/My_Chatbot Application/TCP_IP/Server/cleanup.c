12826bba85741bd23961a1acd7ff088131dbd836
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-09-14 13:06:53 +0530

    Update cleanup.c

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/cleanup.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/cleanup.c
@@ -3,13 +3,13 @@
 // Function defination for cleanup()
 void cleanup() 
 {   
-    int server_sockfd;  // variable used for storing the socket fd
+        int server_sockfd;  // variable used for storing the socket fd
 
-    close(server_sockfd);  // Closing teh socket fd
+        close(server_sockfd);  // Closing teh socket fd
     
-    // Traversing through the list of client's connected stored in the total_list of structures
-    for(int clean = 0; clean < server.total_client; clean++)  
-    {
-        close(server.client_list[clean].file_des);  // Clears the list of conneted clients
-    }
+        // Traversing through the list of client's connected stored in the total_list of structures
+        for(int clean = 0; clean < server.total_client; clean++)  
+        {
+                close(server.client_list[clean].file_des);  // Clears the list of conneted clients
+        }
 }
