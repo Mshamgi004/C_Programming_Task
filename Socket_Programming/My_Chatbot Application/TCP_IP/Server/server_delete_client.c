b908312734e7cd6fe9c6ea27771940db0cbaf34a
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-09-19 12:43:18 +0530

    Use of array shifting so as to gracefully exit the application for client disconnection

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/server_delete_client.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/server_delete_client.c
@@ -7,17 +7,21 @@ void server_delete_client(int socket_fd_delete)
 	int delete = 0;
 	int index = 0;
 
-	// Iterating the variable till tital no. of clients
+	// Iterating the total fd's through whole structure total_list
 	for (delete = 0; delete < NO_OF_CLIENTS; delete++)
 	{
-		// Condition for deleting the client
+		// Condition for deleting the client by checking teh socket_fd_delete is present on structure
 		if (server.client_list[delete].file_des == socket_fd_delete)
 		{
-			server.total_client--;
-			printf("\t------------------Connected client deleted fd : [%d]----------------------------\n",socket_fd_delete);
-			//printf("\t------------------Name of Connected client deletd : [%s]------------------------\n",client_buffer);
-		}
+			// traversing through all the index values for deleting the client
+			for(index = delete; index < NO_OF_CLIENTS; index++)
+			{
+				//Array indexing shifting when there is client present in the structure
+				server.client_list[index] = server.client_list[index + 1];
+			}
+		}	
 	}
-
+	server.total_client--;
+	printf("\t------------------Connected client deleted fd : [%d]----------------------------\n",socket_fd_delete);
 	close(socket_fd_delete);
 }
