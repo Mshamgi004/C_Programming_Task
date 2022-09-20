7d7e522dd94b44df6e9c39ea287e8fde59f7973e
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-09-20 12:39:12 +0530

    Update client_select.c

--- Socket_Programming/My_Chatbot Application/TCP_IP/Client/client_select.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Client/client_select.c
@@ -1,35 +1,40 @@
-// Function declaration for client_select
 #include "client.h"
 
-int client_select(int maxval_fd,int client_sockfd, fd_set *readfds, fd_set *writefds) 
+// Function declaration for client_select
+int client_select(int maxval_fd, int client_sockfd, fd_set* readfds, fd_set* writefds)
 {
 	// Declaring the char buffer for storing the sending and recieving message
-	char recv_msgg[MAX_BUFFER_SIZE] ;
-	char send_buff[MAX_BUFFER_SIZE] ;
-	memset(recv_msgg, 0 ,sizeof(recv_msgg));
-	memset(send_buff, 0 ,sizeof(send_buff));
-    
-	int client_action = select(maxval_fd+1,readfds,writefds,NULL,NULL);   // select() system call 
-	
-	// To check if select() sys call is working or not
-	if(client_action == -1 || client_action == 0) 
+	char recv_msgg[MAX_BUFFER_SIZE];
+	char send_buff[MAX_BUFFER_SIZE];
+	memset(recv_msgg, 0, sizeof(recv_msgg));
+	memset(send_buff, 0, sizeof(send_buff));
+
+	int action = select(maxval_fd + 1, readfds, writefds, NULL, NULL);   // select() system call 
+
+	// To check if the select() sys call is working or not
+	if (action == -1 || action == 0)
 	{
-		printf("****ERROR: select()****\n");
+		printf("\t||***************************ERROR: select()******************************||\n");
 		exit(0);
-    	}
+	}
 	// To check for all the clients in the file des inside the readfds
-    	if(FD_ISSET(client_sockfd,readfds)) 
-    	{	
+	if (FD_ISSET(client_sockfd, readfds))
+	{
 		// If its present in readfds then it will recieve the message
-		client_recv_from_server(client_sockfd,recv_msgg);             
-    	}
+		client_recv_from_server(client_sockfd, recv_msgg);
+	}
 	// To check the fd is set for sending the message so as to broadcast the message to all clients
-   	if(FD_ISSET(STDIN_FILENO,readfds)) 
-    	{
-		if(read(0,send_buff,sizeof(send_buff)) > 0) 
+	if (FD_ISSET(STDIN_FILENO, readfds))
+	{
+		if (read(0, send_buff, sizeof(send_buff)) > 0)
 		{
-            		client_send_to_server(client_sockfd,send_buff);
-        	}
-   	}
+			client_send_to_server(client_sockfd, send_buff);
+		}
+		else
+		{
+			printf("\tNot able to enter the send_to_server\n");
+		}
+	}
 	return 0;
 }
+
