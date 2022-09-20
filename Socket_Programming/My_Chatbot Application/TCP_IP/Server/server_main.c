3372a1bcbe8e00668ef87810292186f51a5e9b93
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-09-20 12:13:45 +0530

    Update server_main.c

--- Socket_Programming/My_Chatbot Application/TCP_IP/Server/server_main.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Server/server_main.c
@@ -2,27 +2,32 @@
 
 int main()
 {	
-    	// fd_set for reading, writing and exception for socket fd
+	//struct server_data server;
+    
+	// fd_set for reading, writing and exception for socket fd
 	fd_set readfds;
 	fd_set writefds;
 	fd_set exceptfds;
-    	int server_sockfd;
+    
+	int server_sockfd;  // server-sockfd varibale use for creating the server_sockfd 
 
 	int maxval_fd = 0;   // A max sock_fd declared to hold the server_sockfd value
 
 	memset(&server, 0, sizeof(struct server_data));   // Clearing the structure to hold the total clients
+	//memset(&values, 0, sizeof(struct client));
+	
 	printf("\t||---------------------------SERVER STARTED-------------------------------||\n");
 	printf("\t||************************************************************************||\n");
 	
 
 	if (setup_server(&server_sockfd) != 0)   // Function call for setting up of server
 	{
-		printf("*****ERROR : Creation socket failed******");
+		printf("\t||*******************ERROR : Creation socket failed***********************||\n");
 		exit(0);
 	}
 	else
 	{
-		maxval_fd = server_sockfd;   // Equalising the max_fd to server_fd
+		maxval_fd = server_sockfd;   // Putting the values of server_sockfd to maxval_fd
 	}
 
 	while (1)
@@ -32,7 +37,7 @@ int main()
 	}
 
 	cleanup();
-	printf("Bye From server\n");
+	//printf("Bye From server\n");
 
 	return 0;
 }
