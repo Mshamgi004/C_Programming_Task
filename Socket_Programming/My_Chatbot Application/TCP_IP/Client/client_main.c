04288c16516d4b18ca072af1dfa1fceb1ab8efe4
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-09-20 12:38:16 +0530

    Update client_main.c

--- Socket_Programming/My_Chatbot Application/TCP_IP/Client/client_main.c
+++ Socket_Programming/My_Chatbot Application/TCP_IP/Client/client_main.c
@@ -1,7 +1,7 @@
 #include "client.h"
 
-//int main(int argc, char* argv[])
-int main()
+int main(int argc, char* argv[])
+//int main()
 {
 	//char get_name[1024];
 	int client_sockfd = 0;   // Declaring the client_sockfd
@@ -12,8 +12,20 @@ int main()
 	fd_set writefds;
 	fd_set exceptfds;
 
-	printf("Please enter your name: ");                  // Entering the name from client side.
-	fgets(client_name, MAX_BUFFER_SIZE, stdin);          // fgets() used to recieve the name of the client
+	//char buffer1[1000];         // char buffer[] used to store the name
+	//char buffer2[1000];         // char buffer[] used to store the password
+
+	// printf("Please enter your name: ");                  
+	// scanf("%s", buffer1);                           // Entering the name from client side.
+	//fgets(buffer, 1000, stdin);
+	//strcpy(client_name, buffer1);            // Copying the recived name in buffer and sending it to the client_name
+	
+	// printf("\tYou are logged in !!!. Create a password for registration\n");
+	// scanf("%s",buffer2);
+
+
+	// strcpy(password, buffer2);
+	//fgets(client_name, MAX_BUFFER_SIZE, stdin);          // fgets() used to recieve the name of the client
 	// if(fgets(client_name, MAX_BUFFER_SIZE, stdin) != NULL)
 	// {
 	// 	char *array;
@@ -34,11 +46,19 @@ int main()
 	// 	printf("\nName must be more than one and less than ");
 	// }
 	//To check the name given from cmd in binary
-	// if (argc > 2)
-	// {
-	// 	printf("****ERROR : Parameters error****");
-	// 	exit(0);
-	// }
+	if (argc > 2)
+	{
+		printf("\t||**********************ERROR : Parameters error****************************\n");
+		exit(0);
+	}
+
+	strcpy(client_name, argv[1]);
+
+	// printf("\tYou are logged in !!!. Create a password for registration\n");
+	// scanf("%s",buffer2);
+
+
+	// strcpy(password, buffer2);
 	// printf("Enter your name: \n");
 	// fgets(get_name, 1024, stdin);
 	// printf("Name = %s\n", get_name);
@@ -53,7 +73,7 @@ int main()
 
 	if (client_create_socket(&client_sockfd) != 0) // Function call for setting up of client
 	{
-		printf("*****ERROR : Socket creation failed*****");
+		printf("\t||*******************ERROR : Socket creation failed*************************\n");
 		exit(0);
 	}
 	else
